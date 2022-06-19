304c06f14da7076e757719c0ab795107407ba67e
fearandesire <nexusmadethis@Gmail.com>
2022-06-19 18:52:12 -0400

    prettier setup

--- commands/balance.js
+++ commands/balance.js
@@ -1,34 +1,32 @@
-import { Command } from '@sapphire/framework';
-import { Log } from '../utils/ConsoleLogging.js';
-import { checkbalance } from '../utils/checkbalance.js';
+import { Command } from '@sapphire/framework'
+import { checkbalance } from '../utils/checkbalance.js'
+import { Log } from '../utils/ConsoleLogging.js'
 export class balance extends Command {
-    constructor(context, options) {
-        super(context, {
-            ...options,
-            name: 'balance',
-            aliases: ['balance', 'cb', 'bal'],
-            description: 'check balance',
-            requiredUserPermissions: ['KICK_MEMBERS']
-        });
-    }
-    async messageRun(message, args) {
-        const targetbalance = await args.rest('string').catch(()=>null) 
-        Log.LogBorder();
-        Log.LogYellow(`[balance.js] Running Test Promise!`)
-        const userid = message.author.id;
-        if (targetbalance != null){ // if user is checking their own balance 
-        const userid = targetbalance; // changes target id to the User ID to be initiated.
-        checkbalance(userid, message); // reinstalls checkbalance function 
-       return
-        }
-        if (targetbalance != userid) {// if users checking another users balance 
-        const notuser = true
-        checkbalance(userid, message,notuser);
-        return
-        }
-        
-    }
+	constructor(context, options) {
+		super(context, {
+			...options,
+			name: 'balance',
+			aliases: ['balance', 'cb', 'bal'],
+			description: 'check balance',
+			requiredUserPermissions: ['KICK_MEMBERS'],
+		})
+	}
+	async messageRun(message, args) {
+		const targetbalance = await args.rest('string').catch(() => null)
+		Log.LogBorder()
+		Log.LogYellow(`[balance.js] Running Test Promise!`)
+		const userid = message.author.id
+		if (targetbalance != null) {
+			// if user is checking their own balance
+			const userid = targetbalance // changes target id to the User ID to be initiated.
+			checkbalance(userid, message) // reinstalls checkbalance function
+			return
+		}
+		if (targetbalance != userid) {
+			// if users checking another users balance
+			const notuser = true
+			checkbalance(userid, message, notuser)
+			return
+		}
+	}
 }
-
-
-
