00b59016fadca00bd04729f954ad54858ce11769
fearandesire <63979371+fearandesire@users.noreply.github.com>
2022-10-02 02:38:28 -0400

    game day embed + cron job

--- listeners/ready.js
+++ listeners/ready.js
@@ -3,7 +3,7 @@ import { Log } from '#LogColor'
 import { completedReq } from '../utils/api/completedReq.js'
 import { createRequire } from 'module'
 import { embedReply } from '#config'
-import { resolveToday } from './../utils/date/resolveToday.js'
+import { gameDayCron } from '#botUtil/gameDayCron'
 import { scheduleReq } from '#api/scheduleReq'
 
 const require = createRequire(import.meta.url)
@@ -28,19 +28,6 @@ export class ReadyListener extends Listener {
 }
 
 setTimeout(async () => {
-    var todayInfo = await new resolveToday()
-    let completedCheckTime
-    let gameDay
-    if (todayInfo.dayOfWeek.toLowerCase() === `sun`) {
-        completedCheckTime = `<t:1664132400:t>` //* 3:00 PM EST
-        gameDay = true
-    } else if (todayInfo.dayOfWeek.toLowerCase() === `mon`) {
-        completedCheckTime = `<t:1664244000:T>` //* 2:00 PM EST
-        gameDay = true
-    } else if (todayInfo.dayOfWeek.toLowerCase() === `thur`) {
-        completedCheckTime = `<t:1664244000:T>` //* 10:00 PM EST
-        gameDay = true
-    }
     await scheduleReq().then(() => {
         var embedObj = {
             title: `Schedule Queue`,
@@ -51,17 +38,10 @@ setTimeout(async () => {
         }
         embedReply(null, embedObj)
     })
-    if (gameDay === true) {
-        await completedReq().then(() => {
-            var embedObj = {
-                title: `Game Day Queue`,
-                description: `Initiated the game completed queue!\nChecking for completed games today: **every 5 minutes after ${completedCheckTime}**`,
-                color: '#00ff00',
-                target: 'modBotSpamID',
-                footer: 'Pluto | Designed by FENIX#7559',
-            }
-            embedReply(null, embedObj)
-        })
-    }
+    await completedReq().then(() => {
+        Log.Green(`Game Complete Check Cron Que Initiated`)
+    })
+    await gameDayCron()
 }, 5000)
+
 Log.Green(`[Startup]: On-Load Processes started!`)
