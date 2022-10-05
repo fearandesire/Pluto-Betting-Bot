8345309f0b8843319558478096f73690f22bc279
fearandesire <63979371+fearandesire@users.noreply.github.com>
2022-10-05 15:18:24 -0400

    rm commenting

--- utils/db/validation/isMatchExist.js
+++ utils/db/validation/isMatchExist.js
@@ -3,9 +3,9 @@ import { db } from '#db'
 /**
  * @module isMatchExist -
  * Query promise to the DB resolves to a single row if we find a match in the 'activematchups' table.
- * Finds the match by the team name ({@link teamid}) provided by the user.
- * @param {string} teamid - The team the team the user wants to bet on
- * @returns {obj} [1] Row from the query, or null if a matchup containing the team does not exist.
+ * Finds the match by the team name ({@link teamid}) provided.
+ * @param {string} teamid - The team name to search for in the DB
+ * @returns {obj} [1] Row from the query, or null if no match is found
  * @references {@link setupBet.js} - This module is called from setupBet.js - a function that is used to process a new bet.
  */
 
