6b0178ae04788c1db3809fe8d7b17352606f96d1
fearandesire <63979371+fearandesire@users.noreply.github.com>
2022-10-11 12:23:51 -0400

    Patch for cancelling bets + more
    + Prevent cancelling bets from active games
    + Leaderboard shows which position the user is at in the footer of the embed
    + Camel case queryBets

--- utils/date/resolveIso.js
+++ utils/date/resolveIso.js
@@ -7,32 +7,32 @@ import currentWeekNumber from 'current-week-number'
  */
 
 export function resolveIso(isoDate) {
-	this.isoDate = isoDate
-	this.apiDate = new Date(isoDate)
-	var localDate = this.apiDate.toLocaleString()
-	var localDateSplit = localDate.split(`,`)
-	var localDateSplit2 = localDateSplit[1].split(`:`)
-	var dayNum = this.apiDate.getDate()
-	var region = `en-US`
-	var options = { timeStyle: `short`, hourCycle: `h23` }
-	var timeFormatted = this.apiDate.toLocaleString(region, options)
-	this.timeRaw = timeFormatted.toString().replace(`:`, ``)
-	this.dayNum = this.apiDate.getDate()
-	this.month = this.apiDate.getMonth() + 1
-	this.year = this.apiDate.getFullYear()
-	this.hour = parseInt(timeFormatted.split(`:`)[0])
-	this.minute = parseInt(timeFormatted.split(`:`)[1])
-	this.apiDateFull = `${this.month} ${this.dayNum}, ${this.year}`
-	this.weekNum = currentWeekNumber(this.apiDateFull)
-	this.dayOfWeek = this.apiDate.toLocaleString(`en-US`, { weekday: `short` })
-	this.rawDate = parseInt(`${this.dayNum}${this.hour}${this.minute}`)
-	this.startTime = {
-		hour: this.hour,
-		minute: this.minute,
-		dayNum: this.dayNum,
-		dayOfWeek: this.dayOfWeek,
-		monthNum: this.month,
-		yearNum: this.year,
-		weekNum: this.apiWeekNum,
-	}
+    this.isoDate = isoDate
+    this.apiDate = new Date(isoDate)
+    var localDate = this.apiDate.toLocaleString()
+    var localDateSplit = localDate?.split(`,`)
+    var localDateSplit2 = localDateSplit[1]?.split(`:`)
+    var dayNum = this.apiDate.getDate()
+    var region = `en-US`
+    var options = { timeStyle: `short`, hourCycle: `h23` }
+    var timeFormatted = this.apiDate.toLocaleString(region, options)
+    this.timeRaw = timeFormatted.toString().replace(`:`, ``)
+    this.dayNum = this.apiDate.getDate()
+    this.month = this.apiDate.getMonth() + 1
+    this.year = this.apiDate.getFullYear()
+    this.hour = parseInt(timeFormatted.split(`:`)[0])
+    this.minute = parseInt(timeFormatted.split(`:`)[1])
+    this.apiDateFull = `${this.month} ${this.dayNum}, ${this.year}`
+    this.weekNum = currentWeekNumber(this.apiDateFull)
+    this.dayOfWeek = this.apiDate.toLocaleString(`en-US`, { weekday: `short` })
+    this.rawDate = parseInt(`${this.dayNum}${this.hour}${this.minute}`)
+    this.startTime = {
+        hour: this.hour,
+        minute: this.minute,
+        dayNum: this.dayNum,
+        dayOfWeek: this.dayOfWeek,
+        monthNum: this.month,
+        yearNum: this.year,
+        weekNum: this.apiWeekNum,
+    }
 }
---
+++ utils/db/betOps/verifyCancellation.js
@@ -0,0 +1,20 @@
+import { gameActive } from '#dateUtil/gameActive'
+import { db } from '#db'
+/**
+ * @module verifyCancellation
+ * Retrieve the team name (teamid) from the provided bet id and check if the game has started. Intended to prevent cancellation of a bet from a game that has already started.
+ */
+
+export async function verifyCancellation(userid, betId) {
+    var bet = await db.oneOrNone(
+        `SELECT * FROM activebets WHERE betid = $1 AND userid = $2`,
+        [betId, userid],
+    )
+    var teamName = bet.teamid
+    var gameStatus = await gameActive(teamName)
+    if (gameStatus) {
+        return true
+    } else {
+        return false
+    }
+}
