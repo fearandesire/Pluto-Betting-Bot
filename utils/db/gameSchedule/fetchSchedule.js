2eb011c2e36aa0de01dd232baf2e02cf2b49f4d0
fearandesire <63979371+fearandesire@users.noreply.github.com>
2022-10-18 18:44:26 -0400

    Catch null matchups in schedule check

--- utils/db/gameSchedule/fetchSchedule.js
+++ utils/db/gameSchedule/fetchSchedule.js
@@ -28,20 +28,28 @@ export async function fetchSchedule(interaction) {
         })
         return
     }
-    await db.manyOrNone(`SELECT * FROM activematchups`).then(async (data) => {
-        if (_.isEmpty(data)) {
-            Log.Red(`No active matchups found in the database.`)
-            return
-        } else {
-            container.numOfMatchups = data.length
-            for await (let [key, value] of Object.entries(data)) {
-                var homeTeam = value.teamone
-                var awayTeam = value.teamtwo
-                var startTime = value.cronstart
-                await scheduleChannels(homeTeam, awayTeam, startTime)
+    var checkDB = await db
+        .manyOrNone(`SELECT * FROM activematchups`)
+        .then(async (data) => {
+            if (_.isEmpty(data)) {
+                Log.Red(`No active matchups found in the database.`)
+                return false
+            } else {
+                container.numOfMatchups = data.length
+                for await (let [key, value] of Object.entries(data)) {
+                    var homeTeam = value.teamone
+                    var awayTeam = value.teamtwo
+                    var startTime = value.cronstart
+                    await scheduleChannels(homeTeam, awayTeam, startTime)
+                }
             }
-        }
-    })
+        })
+    if (checkDB == false) {
+        await msgBotChan(
+            `No active matchups found in the database, no games have been scheduled.`,
+        )
+        return
+    }
     var embed = new MessageEmbed()
         .setTitle(`Game Channels Queue`)
         .setDescription(
