66f27c2d2b37bede0ae6fca16fa7615e3eeb1f06
fearandesire <63979371+fearandesire@users.noreply.github.com>
2022-10-23 22:27:49 -0400

    leaderboard slice fix pt2

--- utils/db/currency/leaderboard.js
+++ utils/db/currency/leaderboard.js
@@ -31,13 +31,13 @@ export async function leaderboard(message, interactionEph) {
                 var lbEntry = `**${humanIndex}.** ${membersId}: ${lbUserBal}`
                 lbArray.push(lbEntry)
             }
-            lbArray = lbArray.join('\n')
-            var lbString = lbArray.toString()
+            var lbJoined = lbArray.join('\n')
+            var lbString = lbJoined.toString()
             var lbLength = lbString.length
             console.log(`Array Length: ${lbLength}`)
             if (lbLength > 4096) {
-                var lbArr1 = lbString.slice(0, lbArray.length / 2)
-                var lbArr2 = lbString.slice(lbArray.length / 2, lbArray.length)
+                var lbArr1 = lbArray.slice(0, lbArray.length / 2)
+                var lbArr2 = lbArray.slice(lbArray.length / 2, lbArray.length)
                 var half1Data = lbArr1.join('\n')
                 var half2Data = lbArr2.join('\n')
                 var firstHalf = {
