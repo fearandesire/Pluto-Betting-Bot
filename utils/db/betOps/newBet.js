1f06d7aa0e8f01d5e77051cc94841bc0b92178f9
fearandesire <63979371+fearandesire@users.noreply.github.com>
2022-10-25 10:33:47 -0400

    date validation for bet cancellations

--- utils/db/betOps/newBet.js
+++ utils/db/betOps/newBet.js
@@ -9,46 +9,57 @@ import { setupBetLog } from '#winstonLogger'
 import { validateUser } from '#utilValidate/validateExistingUser'
 import { verifyDupBet } from '#utilValidate/verifyDuplicateBet'
 
-export async function newBet(message, betOnTeam, betAmount, interactionEph) {
-    var user = message?.author?.id || message.user.id
-    var userName = message?.author?.tag || message.user.username
+export async function newBet(
+    interaction,
+    betOnTeam,
+    betAmount,
+    interactionEph,
+) {
+    var user = interaction?.author?.id || interaction.user.id
+    var userName = interaction?.author?.tag || interaction.user.username
     betOnTeam = await resolveTeam(betOnTeam)
-    var matchInfo = await resolveMatchup(betOnTeam)
+    var matchInfo = await resolveMatchup(betOnTeam, null)
+    if (!matchInfo) {
+        QuickError(interaction, `Unable to locate a match for ${betOnTeam}`, true)
+        return
+    }
     var matchupId = parseInt(matchInfo.matchupId)
-    var activeCheck = await gameActive(betOnTeam)
-
+    var activeCheck = await gameActive(betOnTeam, matchupId)
     if (!betOnTeam) {
         //# failure to match team
-        QuickError(message, 'Please enter a valid team or match id', true)
+        QuickError(interaction, 'Please enter a valid team or match id', true)
         return
     }
     if (activeCheck == true) {
         QuickError(
-            message,
+            interaction,
             `This match has already started. You are unable to place a bet on active games.`,
             true,
         )
         return
     }
-    // await Log.Yellow(
-    //  `[newBet.js] User ${userName} (${user}) is getting a bet ready!`, //? Debug purposes, this will likely be removed later. For now, it's intended to confirm the user's input
-    // )
-    //await Log.Blue(`Bet Slip:\nAmount: ${betAmount}\nTeam: ${betOnTeam}`)
     await setupBetLog.info(
         `User ${userName} (${user}) is getting a bet ready!\nBet Slip:\nAmount: ${betAmount}\nTeam: ${betOnTeam}`,
     )
     async.series(
         [
             async function valUser() {
-                await validateUser(message, user, true)
+                await validateUser(interaction, user, true)
                 return
             },
             async function verDup() {
-                await verifyDupBet(message, user, matchupId)
+                await verifyDupBet(interaction, user, matchupId)
                 return
             },
             async function setBet() {
-                await setupBet(message, betOnTeam, betAmount, user, interactionEph)
+                await setupBet(
+                    interaction,
+                    betOnTeam,
+                    betAmount,
+                    user,
+                    matchupId,
+                    interactionEph,
+                )
                 return
             },
         ],
