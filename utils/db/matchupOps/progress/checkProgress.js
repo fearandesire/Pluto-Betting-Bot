930bb5f4b34311ca34fd16a3a69a2eb9533d08c9
fearandesire <63979371+fearandesire@users.noreply.github.com>
2022-10-27 19:41:12 -0400

    in progress matchup from cache > DB

---
+++ utils/db/matchupOps/progress/checkProgress.js
@@ -0,0 +1,22 @@
+/**
+ * @module checkProgress
+ * @summary Check if a matchup is in the process of having it's bets closed
+ * @returns {boolean} - True if the matchup is in progress, false if not
+ */
+
+import { inProgress } from './inProgress.js'
+
+export async function checkProgress(homeTeam, awayTeam) {
+	var progCheck = await inProgress(homeTeam, awayTeam).then((res) => {
+		var inProg = res[0]?.inprogress
+		console.log(
+			`[checkProgress.js] ${homeTeam} vs. ${awayTeam}: In Progress Result: ${inProg}`,
+		)
+		if (inProg == true) {
+			return true
+		} else {
+			return false
+		}
+	})
+	return progCheck
+}
