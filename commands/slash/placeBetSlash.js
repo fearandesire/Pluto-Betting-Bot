362072b599104663b1777f180f965821968be8a2
fearandesire <63979371+fearandesire@users.noreply.github.com>
2022-11-04 04:07:22 -0400

    fix for pending bet validation + more
    + Moved pending bet verification to the database
     - Updated the areas calling the prior memory storage version
    + Fixed bet cancellation reply

--- commands/slash/placeBetSlash.js
+++ commands/slash/placeBetSlash.js
@@ -1,5 +1,6 @@
 import { Command } from '@sapphire/framework'
 import { newBet } from '#utilBetOps/newBet'
+import { pendingBet } from '../../utils/db/validation/pendingBet.js'
 
 export class placeBetSlash extends Command {
     constructor(context, options) {
@@ -8,7 +9,7 @@ export class placeBetSlash extends Command {
             name: 'placeBetSlash',
             aliases: [''],
             description:
-                "Place a bet on a matchup. Use the /odds command to view today's NBA Games & Odds!",
+                "Place a bet on a matchup. Use the /odds command to view this week's NFL Games!",
             chatInputCommand: {
                 register: true,
             },
@@ -39,16 +40,43 @@ export class placeBetSlash extends Command {
     async chatInputRun(interaction) {
         //console.log(interaction.user.id)
         var userid = interaction.user.id
-        var betOnTeam = interaction.options.getString('team')
-        var betAmount = interaction.options.getInteger('amount')
-        if (betOnTeam.match(/^[0-9]+$/) && betOnTeam.toLowerCase() !== `76ers`) {
+        var hasPending = await new pendingBet().checkPending(userid)
+        if (hasPending) {
+            interaction.reply({
+                content: `You are already setting up another bet. Please finish that bet before placing another.`,
+                ephemeral: true,
+            })
+            return
+        } else {
+            await new pendingBet().insertPending(userid)
+        }
+        if (!interaction.options.getString('team')) {
+            interaction.reply({
+                content: `**Please provide a team to bet on.**`,
+                ephemeral: true,
+            })
+            return
+        }
+        if (interaction.options.getString(`team`).match(/^[0-9]+$/)) {
             interaction.reply({
                 content: `**Please provide a valid team.**`,
                 ephemeral: true,
             })
             return
         }
+        if (!interaction.options.getInteger('amount')) {
+            interaction.reply({
+                content: `**Please provide an amount to bet.**`,
+                ephemeral: true,
+            })
+            return
+        }
         var interactionEph = true
-        await newBet(interaction, betOnTeam, betAmount, interactionEph)
+        await newBet(
+            interaction,
+            interaction.options.getString('team'),
+            interaction.options.getInteger('amount'),
+            interactionEph,
+        )
     }
 }
---
+++ utils/db/validation/pendingBet.js
@@ -0,0 +1,24 @@
+import { db } from '#db'
+
+/**
+ * @module pendingBet
+ * Handle queries to the database regarding pending bets. Note: 'pending bets' here indicate that the user is in the process of closing a bet.
+ * This is to prevent the user from fooling the system and duplicating money.
+ * - Places a user into pending bets
+ * - Checks if a user exists in pending bets
+ */
+
+export function pendingBet() {
+    //# pending bets table is named `pendingABet`
+    this.checkPending = async (userid) => {
+        return db.oneOrNone(`SELECT * FROM "pendingABet" WHERE userid = $1`, [
+            userid,
+        ])
+    }
+    this.insertPending = async (userid) => {
+        return db.none(`INSERT INTO "pendingABet" (userid) VALUES ($1)`, [userid])
+    }
+    this.deletePending = async (userid) => {
+        return db.none(`DELETE FROM "pendingABet" WHERE userid = $1`, [userid])
+    }
+}
