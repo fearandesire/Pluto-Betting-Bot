aa86b4365f530f5406f415e7b54e5a6d6a75dee1
fearandesire <63979371+fearandesire@users.noreply.github.com>
2022-11-27 17:24:42 -0500

    solo color for dm embed

--- utils/bot_res/dmMe.js
+++ utils/bot_res/dmMe.js
@@ -7,36 +7,16 @@ import { SapDiscClient } from '#main'
  */
 
 export async function dmMe(message, embed) {
-    console.log(`sending DM`)
     var userid = process.env.botDevID
     await SapDiscClient.users.fetch(`${userid}`).then((user) => {
         if (embed) {
             user.send({ embeds: [embed] })
         } else {
-            //# hex color array for embeds to use randomly
-            var hexColorArray = [
-                `#3abc2c`,
-                `#f1c40f`,
-                `#e74c3c`,
-                `#3498db`,
-                `#9b59b6`,
-                `#34495e`,
-                `#2ecc71`,
-                `#1abc9c`,
-                `#95a5a6`,
-                `#f39c12`,
-                `#d35400`,
-                `#c0392b`,
-                `#bdc3c7`,
-                `#7f8c8d`,
-            ]
-            var randomColor =
-                hexColorArray[Math.floor(Math.random() * hexColorArray.length)]
             //# compile embed
             var formatEmb = new MessageEmbed()
                 .setTitle(`Notification`)
                 .setDescription(message)
-                .setColor(randomColor)
+                .setColor(`#8080ff`)
             user.send({ embeds: [formatEmb] })
         }
     })
