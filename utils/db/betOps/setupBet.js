f40c8c98c65c63d445782f7b259213e68bc5ad47
fearandesire <63979371+fearandesire@users.noreply.github.com>
2022-11-28 18:47:57 -0500

    hotfix for placing new bets
    New DB Router missing double quotes for capital letter DB tables

--- utils/db/betOps/setupBet.js
+++ utils/db/betOps/setupBet.js
@@ -20,60 +20,60 @@ import { validateData } from './../validation/validateData.js'
  */
 
 export async function setupBet(message, teamName, betamount, user) {
-    new FileRunning(`setupBet`)
-    var dbQuery = qBuilder(NFL_MATCHES, [`teamone`, `teamtwo`], teamName)
-    await new validateData(dbQuery).uniqueRowOr().then(async (data) => {
-        //? if team user wishes to bet on exists in the matchups DB, Do:
-        if (data) {
-            //# verify user has funds for the bet
-            var checkFunds = await fetchBalance(message, user)
-            if (!checkFunds) {
-                QuickError(
-                    message,
-                    `Unable to locate any balance for your account in the database.`,
-                    true,
-                )
-                var userName = message?.author?.tag || message.user.username
-                throw new Error(
-                    `User ${userName} (${user}) does not have sufficient funds to place their bet. [Unable to locate any balance for your account in the database]`,
-                )
-            } else if (checkFunds < betamount) {
-                await new pendingBet().deletePending(user)
-                var embedcontent = {
-                    title: 'Insufficient Funds',
-                    description: `You do not have sufficient funds to place this bet. Your current balance is $**${checkFunds}**`,
-                    color: 'RED',
-                    target: `reply`,
-                }
-                await embedReply(message, embedcontent, true)
-                throw Log.Error(
-                    `User ${user} does not have sufficient funds to place their desired bet ${betamount} on ${teamName}.\n Retrieved Balance: ${checkFunds}`,
-                )
-            }
+	new FileRunning(`setupBet`)
+	var dbQuery = qBuilder(`${NFL_MATCHES}`, [`teamone`, `teamtwo`], teamName)
+	await new validateData(dbQuery).uniqueRowOr().then(async (data) => {
+		//? if team user wishes to bet on exists in the matchups DB, Do:
+		if (data) {
+			//# verify user has funds for the bet
+			var checkFunds = await fetchBalance(message, user)
+			if (!checkFunds) {
+				QuickError(
+					message,
+					`Unable to locate any balance for your account in the database.`,
+					true,
+				)
+				var userName = message?.author?.tag || message.user.username
+				throw new Error(
+					`User ${userName} (${user}) does not have sufficient funds to place their bet. [Unable to locate any balance for your account in the database]`,
+				)
+			} else if (checkFunds < betamount) {
+				await new pendingBet().deletePending(user)
+				var embedcontent = {
+					title: 'Insufficient Funds',
+					description: `You do not have sufficient funds to place this bet. Your current balance is $**${checkFunds}**`,
+					color: 'RED',
+					target: `reply`,
+				}
+				await embedReply(message, embedcontent, true)
+				throw Log.Error(
+					`User ${user} does not have sufficient funds to place their desired bet ${betamount} on ${teamName}.\n Retrieved Balance: ${checkFunds}`,
+				)
+			}
 
-            var oddsForTeam = await resolveMatchup(teamName, `odds`)
-            var potentialPayout = await resolvePayouts(oddsForTeam, betamount)
-            //? 'betslip' - object containing the user's bet information
-            var betslip = {}
-            betslip.userid = user
-            betslip.amount = betamount
-            betslip.teamid = teamName
-            betslip.payout = potentialPayout.payout
-            betslip.profit = potentialPayout.profit
-            await confirmBet(message, betslip, user) //# ask user to confirm their bet
-            return true
-            //? Otherwise, throw error
-        } else {
-            var embObj = {
-                title: 'Bet Error',
-                description: `Team containing ${teamName} is not available to bet on. Please review the active matchups.`,
-                color: 'RED',
-                silent: true,
-            }
-            await embedReply(message, embObj)
-            throw Log.Error(
-                `[setupBet.js] Team containing ${teamName} does not exist in the database`,
-            )
-        }
-    })
+			var oddsForTeam = await resolveMatchup(teamName, `odds`)
+			var potentialPayout = await resolvePayouts(oddsForTeam, betamount)
+			//? 'betslip' - object containing the user's bet information
+			var betslip = {}
+			betslip.userid = user
+			betslip.amount = betamount
+			betslip.teamid = teamName
+			betslip.payout = potentialPayout.payout
+			betslip.profit = potentialPayout.profit
+			await confirmBet(message, betslip, user) //# ask user to confirm their bet
+			return true
+			//? Otherwise, throw error
+		} else {
+			var embObj = {
+				title: 'Bet Error',
+				description: `Team containing ${teamName} is not available to bet on. Please review the active matchups.`,
+				color: 'RED',
+				silent: true,
+			}
+			await embedReply(message, embObj)
+			throw Log.Error(
+				`[setupBet.js] Team containing ${teamName} does not exist in the database`,
+			)
+		}
+	})
 }
