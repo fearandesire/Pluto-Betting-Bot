2433ff18dce3f583383d744ce06d1439834020f6
fearandesire <63979371+fearandesire@users.noreply.github.com>
2022-12-24 13:30:49 -0500

    schedule games hour early

--- utils/db/gameSchedule/scheduleChannels.js
+++ utils/db/gameSchedule/scheduleChannels.js
@@ -4,14 +4,13 @@ import { createChannel } from './createChannel.js'
 import { createRequire } from 'module'
 import { getShortName } from './../../bot_res/getShortName.js'
 import { scheduleChanLog } from '#winstonLogger'
-
+import { cronMath } from './cronMath.js'
 const require = createRequire(import.meta.url)
 const cron = require('node-cron')
-
 /**
  * @module scheduleChannels
  * Use the Cron Time created from {@link collectOdds} to schedule the game channel creation for each game.
- * If the bot is reset, another function will be made to fetch the cron times from the database and pass them to this function.
+ * If the bot is reset, Pluto will collect the corn jobs from the database and schedule them again.
  */
 
 export async function scheduleChannels(
@@ -20,17 +19,18 @@ export async function scheduleChannels(
     cronStartTime,
     legibleStartTime,
 ) {
+    var newCron = await new cronMath(cronStartTime).subtract(1, `hours`)
     homeTeam = await getShortName(homeTeam)
     awayTeam = await getShortName(awayTeam)
     await Log.Yellow(
-        `Creating a Cron Job to create a game channel for: ${awayTeam} vs ${homeTeam} | Cron Time: ${cronStartTime}`,
+        `Creating a Cron Job to create a game channel for: ${awayTeam} vs ${homeTeam} | Cron Time: ${newCron}`,
     )
     await scheduleChanLog.info(
-        `Creating a Cron Job to create a game channel for: ${awayTeam} vs ${homeTeam} | Cron Time: ${cronStartTime}`,
+        `Creating a Cron Job to create a game channel for: ${awayTeam} vs ${homeTeam} | Cron Time: ${newCron}`,
     )
     var createCron = async () => {
         await cron.schedule(
-            `${cronStartTime}`,
+            `${newCron}`,
             async () => {
                 await createChannel(awayTeam, homeTeam)
             },
@@ -39,10 +39,10 @@ export async function scheduleChannels(
     }
     await createCron().then(async () => {
         await Log.Green(
-            `Successfully created Cron Job to create a game channel for: ${awayTeam} vs ${homeTeam} | Cron Time: ${cronStartTime}`,
+            `Successfully created Cron Job to create a game channel for: ${awayTeam} vs ${homeTeam} | Cron Time: ${newCron}`,
         )
         await scheduleChanLog.info(
-            `Successfully created Cron Job to create a game channel for: ${awayTeam} vs ${homeTeam} | Cron Time: ${cronStartTime}`,
+            `Successfully created Cron Job to create a game channel for: ${awayTeam} vs ${homeTeam} | Cron Time: ${newCron}`,
         )
         gamesScheduled.push(`• ${awayTeam} vs ${homeTeam} | ${legibleStartTime}`)
         return
