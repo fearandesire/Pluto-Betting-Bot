e71e9bda7521ff8d78164a5bf538e0502399d82d
fearandesire <63979371+fearandesire@users.noreply.github.com>
2023-01-16 14:37:26 -0500

    update: clear schedule before inserting new
    The text of the schedule wasn't being cleared at all. Moved it to cache to easily manage it & persist between restarts.

---
+++ utils/cache/fetchScheduleArr.js
@@ -0,0 +1,20 @@
+import flatcache from 'flat-cache'
+/**
+ * @module scheduleArr
+ * Return the array of schedule games from cache. If it doesn't exist yet, this will create it.
+ * @returns {array} Return an empty array, or the array of games themselves.
+ */
+
+export async function fetchScheduleArr() {
+    const schCache = await flatcache.create(
+        `scheduleArr.json`,
+        `./cache/scheduleArr`,
+    )
+    let schArr = (await schCache.getKey(`scheduleArr`)) || null
+    if (!schArr) {
+        await schCache.setKey(`scheduleArr`, [])
+        await schCache.save(true)
+        schArr = await schCache.getKey(`scheduleArr`)
+    }
+    return schArr
+}
