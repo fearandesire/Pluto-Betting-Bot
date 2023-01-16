d462a0882dd485be096029fa626104c8a5444230
fearandesire <63979371+fearandesire@users.noreply.github.com>
2023-01-16 12:45:04 -0500

    hotfix: Refractor gif util, re-fine gif selection

--- utils/db/gameSchedule/gameEmbed.js
+++ utils/db/gameSchedule/gameEmbed.js
@@ -6,6 +6,7 @@ import { resolveTeam } from '#cmdUtil/resolveTeam'
 import { validateData } from './../validation/validateData.js'
 import { LIVEBETS } from '#config'
 import { resolveTeamColor } from '../../cmd_res/resolveTeam.js'
+import { fetchGif } from '#utilBot/fetchGif'
 /**
 
  * @module gameEmbed
@@ -52,108 +53,10 @@ export async function gameEmbed(hometeam, awayteam) {
         : `N/A`
     let favoredTeam = Number(hOdds) < Number(aOdds) ? hTeam : aTeam // If the home team has higher odds, they are favored, otherwise the away team is favored
     let gifLink
-    let giphy
-    let gif
-    // # Obj with all nba teams to match their short name with their full name
-    const nbaTeams = {
-        hawks: 'Atlanta Hawks',
-        celtics: 'Boston Celtics',
-        nets: 'Brooklyn Nets',
-        hornets: 'Charlotte Hornets',
-        bulls: 'Chicago Bulls',
-        cavaliers: 'Cleveland Cavaliers',
-        dallas: 'Dallas Mavericks',
-        nuggets: 'Denver Nuggets',
-        pistons: 'Detroit Pistons',
-        warriors: 'Golden State Warriors',
-        rockets: 'Houston Rockets',
-        pacers: 'Indiana Pacers',
-        wizards: 'Washington Wizards',
-        clippers: 'Los Angeles Clippers',
-        lakers: 'Los Angeles Lakers',
-        grizzlies: 'Memphis Grizzlies',
-        heat: 'Miami Heat',
-        bucks: 'Milwaukee Bucks',
-        magic: 'Orlando Magic',
-        knicks: 'New York Knicks',
-        thunder: 'Oklahoma City Thunder',
-        '76ers': 'Philadelphia 76ers',
-        suns: 'Phoenix Suns',
-        'trail-blazers': 'Portland Trail Blazers',
-        kings: 'Sacramento Kings',
-        spurs: 'San Antonio Spurs',
-        raptors: 'Toronto Raptors',
-        jazz: 'Utah Jazz',
-        pelicans: 'New Orleans Pelicans',
-        timberwolves: 'Minnesota Timberwolves',
-    }
-
-    const nflteams = {
-        cardinals: `Arizona Cardinals`,
-        falcons: `Atlanta Falcons`,
-        ravens: `Baltimore Ravens`,
-        bills: `Buffalo Bills`,
-        panthers: `Carolina Panthers`,
-        bears: `Chicago Bears`,
-        bengals: `Cincinnati Bengals`,
-        browns: `Cleveland Browns`,
-        cowboys: `Dallas Cowboys`,
-        broncos: `Denver Broncos`,
-        lions: `Detroit Lions`,
-        packers: `Green Bay Packers`,
-        texans: `Houston Texans`,
-        colts: `Indianapolis Colts`,
-        jaguars: `Jacksonville Jaguars`,
-        chiefs: `Kansas City Chiefs`,
-        chargers: `Los Angeles Chargers`,
-        raiders: `Las Vegas Raiders`,
-        rams: `Los Angeles Rams`,
-        dolphins: `Miami Dolphins`,
-        vikings: `Minnesota Vikings`,
-        patriots: `New England Patriots`,
-        saints: `New Orleans Saints`,
-        giants: `New York Giants`,
-        jets: `New York Jets`,
-        eagles: `Philadelphia Eagles`,
-        steelers: `Pittsburgh Steelers`,
-        seahawks: `Seattle Seahawks`,
-        buccaneers: `Tampa Bay Buccaneers`,
-        titans: `Tennessee Titans`,
-        redskins: `Washington Redskins`,
-    }
-
     if (favoredTeam === hTeam) {
-        // # Find team full name in property of nba teams.
-        const teamName = nflteams[hTeam.toLowerCase()]
-        const randomTerms = [`${teamName} hype`, `${teamName}`]
-        const selectRandomTerm =
-            randomTerms[Math.floor(Math.random() * randomTerms.length)]
-        // # Query Giphy API with team name + 'hype' and return one of the 5 gifs direct image link.
-        const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHYAPIKEY}&q=${selectRandomTerm}&limit=10&offset=0&rating=pg-13&lang=en`
-        giphy = await fetch(url).then((res) => res.json())
-        // # Select 1 random gif from the 5 returned
-        gif = giphy.data[Math.floor(Math.random() * giphy.data.length)]
-        // # Return the gif's direct image link
-        gifLink = gif.images.original.url
+        gifLink = await fetchGif(hTeam)
     } else if (favoredTeam === aTeam) {
-        // # Find team full name in property of nba teams.
-        const teamName = nbaTeams[aTeam]
-        const randomTerms = [
-            `${teamName} hype`,
-            `${teamName}`,
-            `${teamName} scoring`,
-            `${teamName} dunk`,
-            `${teamName} block`,
-            `${teamName} assist`,
-        ]
-        const selectRandomTerm =
-            randomTerms[Math.floor(Math.random() * randomTerms.length)]
-        const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHYAPIKEY}&q=${selectRandomTerm}&limit=5&offset=0&rating=pg-13&lang=en`
-        giphy = await fetch(url).then((res) => res.json())
-        gif = giphy.data[Math.floor(Math.random() * giphy.data.length)]
-        gifLink = gif.images.original.url
-
-        var totalBetCount = parseInt(hTBetCount) + parseInt(aTBetCount)
+        gifLink = await fetchGif(aTeam)
     }
     var color = await resolveTeamColor(favoredTeam)
     // # collect team emoji
