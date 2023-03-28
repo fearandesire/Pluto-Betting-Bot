08222fafd0559ad32f42ad6c9a25cf1de2740756
fearandesire <63979371+fearandesire@users.noreply.github.com>
2023-03-27 23:35:06 -0400

    Update: Flag for cron math, pass in custom engine
    + Added flag `noMath` which will prevent `cronMath` being run twice in the event of an app restart.
    + Pass in custom Google Search Engine URL from .env config to ``gameEmbed` to supply param to retrieve the image for the embed.

--- utils/db/gameSchedule/gameEmbed.js
+++ utils/db/gameSchedule/gameEmbed.js
@@ -4,11 +4,11 @@ import { findEmoji } from '../../bot_res/findEmoji.js'
 import { resolveMatchup } from '#cacheUtil/resolveMatchup'
 import { resolveTeam } from '#cmdUtil/resolveTeam'
 import { validateData } from '../validation/validateData.js'
-import { LIVEBETS } from '#config'
+import { LIVEBETS, GOOGLE_CUSTOM } from '#config'
 import { resolveTeamColor } from '../../cmd_res/resolveTeam.js'
 import { fetchVsImg } from '#utilBot/fetchVsImg'
-/**
 
+/**
  * @module gameEmbed
  * Create an embed containing information about the teams playing
  * Lists:
@@ -20,59 +20,56 @@ import { fetchVsImg } from '#utilBot/fetchVsImg'
  */
 
 export async function gameEmbed(hometeam, awayteam) {
-    console.log(
-        `Creating game embed information for: (away) ${awayteam} vs (home) ${hometeam}  `,
-    )
-    const hTeam = await resolveTeam(hometeam)
-    const aTeam = await resolveTeam(awayteam)
+	console.log(
+		`Creating game embed information for: (away) ${awayteam} vs (home) ${hometeam}  `,
+	)
+	const hTeam = await resolveTeam(hometeam)
+	const aTeam = await resolveTeam(awayteam)
 
-    // setup objs to query the DB with
-    const hTeamQuery = new validateData({
-        tables: `${LIVEBETS}`,
-        columns: `amount`,
-        where: `teamid`,
-        values: hTeam,
-    })
-    const aTeamQuery = new validateData({
-        tables: `${LIVEBETS}`,
-        columns: `amount`,
-        where: `teamid`,
-        values: aTeam,
-    })
-    const hTBetTotal = (await hTeamQuery.sumAll())?.sum ?? 0
-    const aTBetTotal = (await aTeamQuery.sumAll())?.sum ?? 0
-    const hTBetCount = (await hTeamQuery.count())?.count ?? 0
-    const aTBetCount = (await aTeamQuery.count())?.count ?? 0
-    const hTeamHigh = (await hTeamQuery.topWager())?.max ?? 0
-    const aTeamHigh = (await aTeamQuery.topWager())?.max ?? 0
-    const hOdds = (await resolveMatchup(hTeam, `odds`))
-        ? await resolveMatchup(hTeam, `odds`)
-        : `N/A`
-    const aOdds = (await resolveMatchup(aTeam, `odds`))
-        ? await resolveMatchup(aTeam, `odds`)
-        : `N/A`
-    const favoredTeam = Number(hOdds) < Number(aOdds) ? hTeam : aTeam // If the home team has higher odds, they are favored, otherwise the away team is favored
-    const imgLink = await fetchVsImg(`${hTeam} vs ${aTeam}`)
-    const color = await resolveTeamColor(favoredTeam)
-    // # collect team emoji
-    const teamEmoji = (await findEmoji(favoredTeam)) || ''
-    // collect server/guild icon url
-    const guildIcon = SapDiscClient.guilds.cache
-        .get(`${process.env.server_ID}`)
-        .iconURL()
-    const embObj = new MessageEmbed()
-        .setTitle(`${awayteam} @ ${hometeam}`)
-        .setDescription(
-            `
+	// setup objs to query the DB with
+	const hTeamQuery = new validateData({
+		tables: `${LIVEBETS}`,
+		columns: `amount`,
+		where: `teamid`,
+		values: hTeam,
+	})
+	const aTeamQuery = new validateData({
+		tables: `${LIVEBETS}`,
+		columns: `amount`,
+		where: `teamid`,
+		values: aTeam,
+	})
+	const hOdds = (await resolveMatchup(hTeam, `odds`))
+		? await resolveMatchup(hTeam, `odds`)
+		: `N/A`
+	const aOdds = (await resolveMatchup(aTeam, `odds`))
+		? await resolveMatchup(aTeam, `odds`)
+		: `N/A`
+	const favoredTeam = Number(hOdds) < Number(aOdds) ? hTeam : aTeam // If the home team has higher odds, they are favored, otherwise the away team is favored
+	const imgLink = await fetchVsImg(`${hTeam} vs ${aTeam}`, GOOGLE_CUSTOM).catch(
+		(err) => console.error(err),
+	)
+	await console.log(`Img Link generated: ${imgLink}`)
+	const color = await resolveTeamColor(favoredTeam)
+	// # collect team emoji
+	const teamEmoji = (await findEmoji(favoredTeam)) || ''
+	// collect server/guild icon url
+	const guildIcon = SapDiscClient.guilds.cache
+		.get(`${process.env.server_ID}`)
+		.iconURL()
+	const embObj = new MessageEmbed()
+		.setTitle(`${awayteam} @ ${hometeam}`)
+		.setDescription(
+			`
 **The ${teamEmoji} ${favoredTeam} are favored to win this game!**
 
 *Type \`/about\` in the <#${process.env.bettingChan}> channel for information about Pluto*`,
-        )
-        .setFooter(``)
-        .setColor(`${color}`)
-        .setThumbnail(`${guildIcon}`)
-        if (imgLink) {
-            embObj.setImage(`${imgLink}`)
-        }
-    return embObj
+		)
+		.setFooter(``)
+		.setColor(`${color}`)
+		.setThumbnail(`${guildIcon}`)
+	if (imgLink) {
+		embObj.setImage(`${imgLink}`)
+	}
+	return embObj
 }
--- utils/db/gameSchedule/scheduleChannels.js
+++ utils/db/gameSchedule/scheduleChannels.js
@@ -17,61 +17,66 @@ const cron = require('node-cron')
  */
 
 export async function scheduleChannels(
-    homeTeam,
-    awayTeam,
-    cronStartTime,
-    legibleStartTime,
+	homeTeam,
+	awayTeam,
+	cronStartTime,
+	legibleStartTime,
+	noMath,
 ) {
-    await console.log(
-        `Scheduling channel:\n${homeTeam} vs ${awayTeam} | ${cronStartTime} | ${legibleStartTime}`,
-    )
-    const schCache = await flatcache.create(
-        `scheduleArr.json`,
-        `./cache/scheduleArr`,
-    )
-    let schArr = (await schCache.getKey(`scheduleArr`)) || null
-    if (!schArr) {
-        await schCache.setKey(`scheduleArr`, [])
-        await schCache.save(true)
-        schArr = await schCache.getKey(`scheduleArr`)
-    }
-    await console.log(`scheduleArr Array -->\n`, schArr)
-    const newCron = await new cronMath(cronStartTime).subtract(1, `hours`)
-    homeTeam = await getShortName(homeTeam)
-    awayTeam = await getShortName(awayTeam)
-    await Log.Yellow(
-        `Creating a Cron Job to create a game channel for: ${awayTeam} vs ${homeTeam} | Cron Time: ${newCron}`,
-    )
-    await scheduleChanLog.info(
-        `Creating a Cron Job to create a game channel for: ${awayTeam} vs ${homeTeam} | Cron Time: ${newCron}`,
-    )
-    const createCron = async () => {
-        await cron.schedule(
-            `${newCron}`,
-            async () => {
-                await createChannel(awayTeam, homeTeam)
-            },
-            { timezone: 'America/New_York' },
-        )
-    }
-    await createCron().then(async () => {
-        await Log.Green(
-            `Successfully created Cron Job to create a game channel for: ${awayTeam} vs ${homeTeam} | Cron Time: ${newCron}`,
-        )
-        await scheduleChanLog.info(
-            `Successfully created Cron Job to create a game channel for: ${awayTeam} vs ${homeTeam} | Cron Time: ${newCron}`,
-        )
-        // # Get first num in legibleStartTime string and subtract 1
-        let firstNum = legibleStartTime.match(/\d+/)
-        firstNum = Number(firstNum[0])
-        const adjNum = firstNum - 1
-        // # Replace it in the string
-        const replaced = legibleStartTime.replace(firstNum, adjNum)
-        const schStr = `• ${awayTeam} vs ${homeTeam} | ${replaced}`
-        const dupeCheck = _.find(schArr, (o) => o === schStr)
-        if (!dupeCheck) {
-            await schArr.push(schStr)
-        }
-        await schCache.save(true)
-    })
+	await console.log(
+		`Scheduling channel:\n${homeTeam} vs ${awayTeam} | ${cronStartTime} | ${legibleStartTime}`,
+	)
+	const schCache = await flatcache.create(
+		`scheduleArr.json`,
+		`./cache/scheduleArr`,
+	)
+	let schArr = (await schCache.getKey(`scheduleArr`)) || null
+	if (!schArr) {
+		await schCache.setKey(`scheduleArr`, [])
+		await schCache.save(true)
+		schArr = await schCache.getKey(`scheduleArr`)
+	}
+	let newCron
+	if (noMath === false) {
+		newCron = await new cronMath(cronStartTime).subtract(1, `hours`)
+	} else {
+		newCron = cronStartTime
+	}
+	homeTeam = await getShortName(homeTeam)
+	awayTeam = await getShortName(awayTeam)
+	await Log.Yellow(
+		`Creating a Cron Job to create a game channel for: ${awayTeam} vs ${homeTeam} | Cron Time: ${newCron}`,
+	)
+	await scheduleChanLog.info(
+		`Creating a Cron Job to create a game channel for: ${awayTeam} vs ${homeTeam} | Cron Time: ${newCron}`,
+	)
+	const createCron = async () => {
+		await cron.schedule(
+			`${newCron}`,
+			async () => {
+				await createChannel(awayTeam, homeTeam)
+			},
+			{ timezone: 'America/New_York' },
+		)
+	}
+	await createCron().then(async () => {
+		await Log.Green(
+			`Successfully created Cron Job to create a game channel for: ${awayTeam} vs ${homeTeam} | Cron Time: ${newCron}`,
+		)
+		await scheduleChanLog.info(
+			`Successfully created Cron Job to create a game channel for: ${awayTeam} vs ${homeTeam} | Cron Time: ${newCron}`,
+		)
+		// # Get first num in legibleStartTime string and subtract 1
+		let firstNum = legibleStartTime.match(/\d+/)
+		firstNum = Number(firstNum[0])
+		const adjNum = firstNum - 1
+		// # Replace it in the string
+		const replaced = legibleStartTime.replace(firstNum, adjNum)
+		const schStr = `• ${awayTeam} vs ${homeTeam} | ${replaced}`
+		const dupeCheck = _.find(schArr, (o) => o === schStr)
+		if (!dupeCheck) {
+			await schArr.push(schStr)
+		}
+		await schCache.save(true)
+	})
 }
