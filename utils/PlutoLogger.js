54035165df02e7d1f5699f33174218b6a63498d9
fearandesire <63979371+fearandesire@users.noreply.github.com>
2023-08-30 14:06:12 -0400

    refactor: :recycle: Add selections to Logger
    Add `id` prop to PlutoLogger to enable selection of titles & color with ease

--- utils/PlutoLogger.js
+++ utils/PlutoLogger.js
@@ -7,16 +7,42 @@ export default class PlutoLogger {
 		const logChan = await SapDiscClient.channels.fetch(
 			logChanID,
 		)
+		let color
+		let title
 		// # Pre-Built Embed for Log Channel
 		const logsEmbed = new MessageEmbed()
-		const color = data?.color || `#ff0000`
-		const title = data?.title || `Logs`
+		switch (data?.id) {
+			case 0:
+				color = '#00ff00' // Green for General Logs
+				title = 'General'
+				break
+			case 1:
+				color = '#0000ff' // Blue for Database Logs
+				title = 'Database'
+				break
+			case 2:
+				color = '#ffff00' // Yellow for Game Scheduling Logs
+				title = 'Game Scheduling'
+				break
+			case 3:
+				color = '#ff00ff' // Magenta for Betting Logs
+				title = 'Betting'
+				break
+			case 4:
+				color = '#ff0000' // Red for Error Logs
+				title = 'Error'
+				break
+			default:
+				color = data?.color || `#ff0000`
+				title = data?.title || `Logs`
+				break
+		}
 		const desc = data?.description || `N/A`
 		const footer = data?.footer || {
 			text: `Pluto | Dev. by fenixforever`,
 		}
 		logsEmbed.setColor(color)
-		logsEmbed.setTitle(title)
+		logsEmbed.setTitle(`${title} Logs`)
 		logsEmbed.setDescription(desc)
 		logsEmbed.setFooter(footer)
 		// # Send embed to modChan
