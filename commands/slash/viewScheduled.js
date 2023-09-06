d9d4a5f2f8a1bf1a113a59a837ffceb366886d60
fearandesire <63979371+fearandesire@users.noreply.github.com>
2023-09-06 11:05:48 -0400

    refactor: :recycle: View scheduled games re-write

--- commands/slash/viewScheduled.js
+++ commands/slash/viewScheduled.js
@@ -1,8 +1,10 @@
 import { Command } from '@sapphire/framework'
 import { MessageEmbed } from 'discord.js'
 import { format } from 'date-fns'
-import { _ } from '#config'
+import { _, QuickError, helpfooter } from '#config'
+import { SPORT } from '#env'
 import Cache from '#rCache'
+import PlutoLogger from '#PlutoLogger'
 
 export class viewScheduled extends Command {
 	constructor(context, options) {
@@ -46,15 +48,73 @@ export class viewScheduled extends Command {
 		}
 
 		const createMatchStr = (game) =>
-			`${game.away_team} *@* ${game.home_team}\n`
+			`${game.away_team} *@* ${game.home_team}`
 
-		// # Create Embed showing scheduled games via fields
+		// Group the scheduled games by day using Lodash's `groupBy` function
+		const groupedGames = _.groupBy(
+			scheduled,
+			(game) => {
+				const gameDate = new Date(game.date)
+				return _.startCase(
+					_.lowerCase(format(gameDate, 'EEEE')),
+				)
+			},
+		)
+		let dayOrder
+
+		if (SPORT === 'nfl') {
+			dayOrder = [
+				'Thursday',
+				'Friday',
+				'Saturday',
+				'Sunday',
+				'Monday',
+				'Tuesday',
+				'Wednesday',
+			]
+		} else if (SPORT === 'nba') {
+			dayOrder = [
+				'Monday',
+				'Tuesday',
+				'Wednesday',
+				'Thursday',
+				'Friday',
+				'Saturday',
+				'Sunday',
+			]
+		} else {
+			await QuickError(
+				interaction,
+				`Unable to view scheduled games - It appears something is setup wrong with the app!`,
+			)
+			await PlutoLogger.log({
+				title: `Game Scheduling Logs`,
+				description: `Error: ${SPORT} is not supported.\nCheck app configuration\nCmd: ${interaction.commandName} | Called By User: ${interaction.user.tag}`,
+			})
+			return
+		}
+
+		// Sort the grouped games by day using Lodash's `orderBy` function
+		const sortedGroupedGames = _.orderBy(
+			Object.entries(groupedGames),
+			([day]) => _.indexOf(dayOrder, day),
+			['asc'],
+		)
+
+		// Create the Discord Embed
 		const emb = new MessageEmbed()
 			.setTitle(`Scheduled Games`)
 			.setColor('#e0ff19')
-		_.forEach(scheduled, (game) => {
-			emb.addField(game.day, createMatchStr(game))
+			.setFooter({ text: helpfooter })
+
+		// Add fields for each day and its corresponding games
+		sortedGroupedGames.forEach(([day, games]) => {
+			const gamesStr = games
+				.map(createMatchStr)
+				.join('\n')
+			emb.addField(day, gamesStr)
 		})
+
 		await interaction.reply({
 			embeds: [emb],
 		})
