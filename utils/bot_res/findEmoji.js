7a10b2b009393608b0a92cb8c03184848e0a38ff
fearandesire <63979371+fearandesire@users.noreply.github.com>
2023-09-16 12:31:12 -0400

    style: :lipstick: Formatting for findEmoji

--- utils/bot_res/findEmoji.js
+++ utils/bot_res/findEmoji.js
@@ -3,26 +3,32 @@ import _ from 'lodash'
 import { SapDiscClient } from '#main'
 
 /**
- * @module findEmoji
- * @summary Find an emoji by name; The matchups usually have an emoji named after each participant.
- * @param {string} inputEmojiName - The name of the emoji to find
- * @var SapDiscClient - The Discord client; Used to cache Discord and locate our emoji's here.
- * @returns {object | null} - The emoji object if found, null if not.
+ * Find an emoji by name and retrieve the closest match.
+ *
+ * @param {string} inputEmojiName - The name of the emoji to search for.
+ * @param {boolean} combine - Whether to combine the emoji with the inputEmojiName.
+ * @return {Promise<string>} A promise that resolves to the closest match emoji or a combined string.
  */
-
 export async function findEmoji(inputEmojiName, combine) {
 	return new Promise(async (resolve, reject) => {
 		let res
 		// Find an emoji by name, retrieve the closest match
 		const emoji =
-			(await SapDiscClient.emojis.cache.find((foundEmoji) => {
-				const lowerEmojiName = _.toLower(inputEmojiName)
-				const lowerEmoji = _.toLower(foundEmoji.name)
-				return (
-					lowerEmoji.includes(lowerEmojiName) ||
-					lowerEmojiName.includes(lowerEmoji)
-				)
-			})) || null
+			(await SapDiscClient.emojis.cache.find(
+				(foundEmoji) => {
+					const lowerEmojiName =
+						_.toLower(inputEmojiName)
+					const lowerEmoji = _.toLower(
+						foundEmoji.name,
+					)
+					return (
+						lowerEmoji.includes(
+							lowerEmojiName,
+						) ||
+						lowerEmojiName.includes(lowerEmoji)
+					)
+				},
+			)) || null
 
 		if (emoji === null && combine) {
 			res = inputEmojiName
