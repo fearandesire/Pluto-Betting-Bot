1d03b9732bd7996044421a6179e883ae0dbadd68
fearandesire <63979371+fearandesire@users.noreply.github.com>
2023-10-10 14:08:50 -0400

    feat: :sparkles: Add `faq` cmd
    `faq` cmd to display information on leveling, tiers and more information in the future

---
+++ commands/faq.js
@@ -0,0 +1,57 @@
+import { Command } from '@sapphire/framework'
+import { helpfooter, embedReply } from '#config'
+import embedColors from '../lib/colorsConfig.js'
+
+export class faq extends Command {
+	constructor(context, options) {
+		super(context, {
+			...options,
+			name: 'faq',
+			description:
+				'Information on frequently asked questions',
+			chatInputCommand: {
+				register: true,
+			},
+		})
+	}
+
+	registerApplicationCommands(registry) {
+		registry.registerChatInputCommand(
+			(builder) =>
+				builder //
+					.setName('faq')
+					.setDescription(this.description),
+			// { idHints: [``] },
+		)
+	}
+
+	async chatInputRun(interaction) {
+		const embObj = {
+			title: `❓ FAQ`,
+			description: `# **__Leveling__**
+			Experience *(XP)* is distributed when bets are closed.
+				- A winning bet is worth 50 XP
+				 - A losing bet is 20.\n
+				Levels are incorporated into the system to establish a structured progression system that goes beyond mere financial gains.
+				This system not only provides a clear measure of who the top performers are but also facilitates rewarding *(prizes, giveaway entries, etc)* the top betters at the end of each season.
+				Everyone starts at level 0, and the max level is 100.
+				
+			## **__Tiers__**
+			Tiers are ranks that you receive as you level up.
+			Here is the list of tiers and their level ranges:
+		    Bronze: 0-15
+			Silver: 15-30
+			Gold: 30-50
+			Emerald: 50-75
+			Diamond: 75-100
+			
+			# **__Parlays__**
+			*TBD*
+			
+			💙 [Support the continued development of Pluto by making a donation](https://ko-fi.com/fenix7559)`,
+			color: embedColors.PlutoBlue,
+			footer: helpfooter,
+		}
+		await embedReply(interaction, embObj)
+	}
+}
