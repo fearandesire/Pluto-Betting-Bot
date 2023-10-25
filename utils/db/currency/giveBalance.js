a0fd33046ba56e2e1da6bb71f55ced4a34036cfc
fearandesire <63979371+fearandesire@users.noreply.github.com>
2023-10-25 18:16:17 -0400

    fix: :bug: Hex code fix

--- utils/db/currency/giveBalance.js
+++ utils/db/currency/giveBalance.js
@@ -1,10 +1,15 @@
 import { Log } from '#LogColor'
 import { SapDiscClient } from '#main'
 import { db } from '#db'
-import { dmLog } from '../../logging.js'
-import { embedReply, CURRENCY, formatCurrency } from '#config'
-import { giveMoneyLog } from '#winstonLogger'
+import {
+	embedReply,
+	CURRENCY,
+	formatCurrency,
+} from '#config'
+import { giveMoneyLog, dmLog } from '#winstonLogger'
+
 import { isInServer } from '../../bot_res/isInServer.js'
+import embedColors from "../../../lib/colorsConfig.js"
 
 /**
  * @module giveBalance - Give money / dollars to a specified user
@@ -14,55 +19,64 @@ import { isInServer } from '../../bot_res/isInServer.js'
  */
 
 export async function giveBalance(
-    interaction,
-    targetUserId,
-    transferammount,
-    interactionEph,
+	interaction,
+	targetUserId,
+	transferammount,
+	interactionEph,
 ) {
-    Log.Yellow(`[transfer.js] Running transfer!`)
+	Log.Yellow(`[transfer.js] Running transfer!`)
 
-    db.tx('processClaim-Transaction', async (t) => {
-        const findUser = await t.oneOrNone(
-            `SELECT * FROM "${CURRENCY}" WHERE userid = $1`,
-            [targetUserId],
-        )
-        var currentBalance = findUser.balance
-        var updatebalance = parseInt(currentBalance) + parseInt(transferammount)
-        var isSilent = interactionEph ? true : false
-        var embObj = {
-            title: `:moneybag: Sent Money`,
-            description: `You have sent ${formatCurrency(transferammount)} to <@${targetUserId}>!`,
-            color: `#00ff40`,
-            silent: isSilent,
-            target: `reply`,
-        }
-        await embedReply(interaction, embObj)
-        await giveMoneyLog.info(
-            `${interaction.user.username} gave ${targetUserId} $${transferammount}!`,
-        )
-        //# DM the user they received money
-        var verifyUser = await isInServer(targetUserId)
-        if (verifyUser) {
-            var receivedMoneyEmb = {
-                title: `:moneybag: Received Money`,
-                description: `You have received $${transferammount} from <@${interaction.user.id}>!`,
-                color: `#00ff40`,
-                footer: ''
-            }
-            await SapDiscClient.users.fetch(`${targetUserId}`).then(async (user) => {
-                if (!user) {
-                    dmLog.error(
-                        `Failed to send DM to user ${targetUserId} is no longer in the server.`,
-                    )
-                    return
-                }
-                await user.send({ embeds: [receivedMoneyEmb] })
-                await dmLog.info(`DM'd ${targetUserId} successfully`)
-            })
-        }
-        return t.any(
-            `UPDATE "${CURRENCY}" SET balance = $1 WHERE userid = $2 RETURNING *`,
-            [updatebalance, targetUserId],
-        )
-    })
+	db.tx('processClaim-Transaction', async (t) => {
+		const findUser = await t.oneOrNone(
+			`SELECT * FROM "${CURRENCY}" WHERE userid = $1`,
+			[targetUserId],
+		)
+		const currentBalance = findUser.balance
+		const updatebalance =
+			Number(currentBalance) + Number(transferammount)
+		const isSilent = !!interactionEph
+		const embObj = {
+			title: `:moneybag: Sent Money`,
+			description: `You have sent ${formatCurrency(
+				transferammount,
+			)} to <@${targetUserId}>!`,
+			color: `${embedColors.PlutoBrightGreen}`,
+			silent: isSilent,
+			target: `reply`,
+		}
+		await embedReply(interaction, embObj)
+		await giveMoneyLog.info(
+			`${interaction.user.username} gave ${targetUserId} $${transferammount}!`,
+		)
+		// # DM the user they received money
+		const verifyUser = await isInServer(targetUserId)
+		if (verifyUser) {
+			const receivedMoneyEmb = {
+				title: `:moneybag: Received Money`,
+				description: `You have received $${transferammount} from <@${interaction.user.id}>!`,
+				color: `#00ff40`,
+				footer: '',
+			}
+			await SapDiscClient.users
+				.fetch(`${targetUserId}`)
+				.then(async (user) => {
+					if (!user) {
+						dmLog.error(
+							`Failed to send DM to user ${targetUserId} is no longer in the server.`,
+						)
+						return
+					}
+					await user.send({
+						embeds: [receivedMoneyEmb],
+					})
+					await dmLog.info(
+						`DM'd ${targetUserId} successfully`,
+					)
+				})
+		}
+		return t.any(
+			`UPDATE "${CURRENCY}" SET balance = $1 WHERE userid = $2 RETURNING *`,
+			[updatebalance, targetUserId],
+		)
+	})
 }
