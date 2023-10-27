79a84e7ba13c8e3678d69f9f1806aed80d5d2de8
fearandesire <63979371+fearandesire@users.noreply.github.com>
2023-10-27 01:08:31 -0400

    refactor: :recycle: Rename function param

--- utils/bot_res/betOps/determineWinner.js
+++ utils/bot_res/betOps/determineWinner.js
@@ -2,16 +2,18 @@
  * @module determineWinner
  * Determine the winner of a game based on the scores. Designed to inspect the API score object of a specific match.
  */
-export async function determineWinner(scoreObj) {
+export async function determineWinner(matchupObj) {
 	// # determine which prop is the home team's score and away team's score by matching the team name
 	let hScoreProp
 	let awScoreProp
-	if (scoreObj.scores[0].name === scoreObj.home_team) {
-		;[hScoreProp, awScoreProp] = scoreObj.scores
-	} else if (
-		scoreObj.scores[0].name === scoreObj.away_team
+	if (
+		matchupObj.scores[0].name === matchupObj.home_team
 	) {
-		;[awScoreProp, hScoreProp] = scoreObj.scores
+		;[hScoreProp, awScoreProp] = matchupObj.scores
+	} else if (
+		matchupObj.scores[0].name === matchupObj.away_team
+	) {
+		;[awScoreProp, hScoreProp] = matchupObj.scores
 	}
 
 	// # determine winner based on the scores
@@ -22,14 +24,14 @@ export async function determineWinner(scoreObj) {
 	let losingTeam
 	let losingTeamHomeOrAway
 	if (Number(homeScore) > Number(awayScore)) {
-		winner = scoreObj.home_team
+		winner = matchupObj.home_team
 		homeOrAwayWon = 'home'
-		losingTeam = scoreObj.away_team
+		losingTeam = matchupObj.away_team
 		losingTeamHomeOrAway = 'away'
 	} else if (Number(homeScore) < Number(awayScore)) {
-		winner = scoreObj.away_team
+		winner = matchupObj.away_team
 		homeOrAwayWon = 'away'
-		losingTeam = scoreObj.home_team
+		losingTeam = matchupObj.home_team
 		losingTeamHomeOrAway = 'home'
 	}
 	return {
