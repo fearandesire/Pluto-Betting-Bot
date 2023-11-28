ab4cc9ab088b83ec88075d52b87ed79c58239bcb
fearandesire <63979371+fearandesire@users.noreply.github.com>
2023-11-28 01:43:55 -0500

    refactor: :lipstick: Change logging borders

--- utils/bot_res/consoleLog.js
+++ utils/bot_res/consoleLog.js
@@ -1,89 +1,89 @@
 import {
-    bgYellowBright,
-    blue,
-    blueBright,
-    bold,
-    green,
-    magentaBright,
-    red,
-    yellow,
-    yellowBright,
+	bgYellowBright,
+	blue,
+	blueBright,
+	bold,
+	green,
+	magentaBright,
+	red,
+	yellow,
+	yellowBright,
 } from 'colorette'
 
-const cborder = `    =========          =========          =========    `
+const cborder = '\u2500'.repeat(30)
 
 export function Border() {
-    return console.log(magentaBright(bold(cborder)))
+	return console.log(magentaBright(bold(cborder)))
 }
 
 export function Green(text) {
-    return console.log(
-        green(bold(`${cborder}\n${text}\n${cborder}`)),
-    )
+	return console.log(
+		green(bold(`${cborder}\n${text}\n${cborder}`)),
+	)
 }
 
 export function Yellow(text) {
-    return console.log(
-        yellow(bold(`${cborder}\n${text}\n${cborder}`)),
-    )
+	return console.log(
+		yellow(bold(`${cborder}\n${text}\n${cborder}`)),
+	)
 }
 
 export function Red(text) {
-    return console.log(
-        red(bold(`${cborder}\n${text}\n${cborder}`)),
-    )
+	return console.log(
+		red(bold(`${cborder}\n${text}\n${cborder}`)),
+	)
 }
 
 export function Blue(text) {
-    return console.log(
-        blue(bold(`${cborder}\n${text}\n${cborder}`)),
-    )
+	return console.log(
+		blue(bold(`${cborder}\n${text}\n${cborder}`)),
+	)
 }
 
 export function BrightBlue(text) {
-    return console.log(
-        blueBright(bold(`${cborder}\n${text}\n${cborder}`)),
-    )
+	return console.log(
+		blueBright(bold(`${cborder}\n${text}\n${cborder}`)),
+	)
 }
 
 export function Magenta(text) {
-    return console.log(
-        magentaBright(
-            bold(`${cborder}\n${text}\n${cborder}`),
-        ),
-    )
+	return console.log(
+		magentaBright(
+			bold(`${cborder}\n${text}\n${cborder}`),
+		),
+	)
 }
 
 export function Error(text) {
-    console.log(red(bold(cborder)))
-    console.log(red(bold(`ERROR: ${text}`)))
-    console.log(red(bold(cborder)))
+	console.log(red(bold(cborder)))
+	console.log(red(bold(`ERROR: ${text}`)))
+	console.log(red(bold(cborder)))
 }
 
 export function CmdPermission(
-    username,
-    userid,
-    commandName,
+	username,
+	userid,
+	commandName,
 ) {
-    console.log(red(bold(cborder)))
-    console.log(
-        red(
-            bold(
-                `PERMISSION ERROR: ${username} (${userid}) attempted to use command ${commandName} they lacked permissions for.`,
-            ),
-        ),
-    )
-    console.log(red(bold(cborder)))
+	console.log(red(bold(cborder)))
+	console.log(
+		red(
+			bold(
+				`PERMISSION ERROR: ${username} (${userid}) attempted to use command ${commandName} they lacked permissions for.`,
+			),
+		),
+	)
+	console.log(red(bold(cborder)))
 }
 
 export const Log = {
-    Border,
-    Green,
-    Yellow,
-    Red,
-    Blue,
-    BrightBlue,
-    Error,
-    CmdPermission,
-    Magenta,
+	Border,
+	Green,
+	Yellow,
+	Red,
+	Blue,
+	BrightBlue,
+	Error,
+	CmdPermission,
+	Magenta,
 }
