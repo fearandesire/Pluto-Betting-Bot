d970af680d124955c78281297efa2bf59fb8621b
fearandesire <63979371+fearandesire@users.noreply.github.com>
2023-12-07 14:01:36 -0500

    style: :art: Format

--- utils/bot_res/AssignIDs.js
+++ utils/bot_res/AssignIDs.js
@@ -1,14 +1,10 @@
-import { Log } from '#LogColor'
 
-//? Util functions to generate unique IDs
 export async function AssignBetID() {
-    const id = Math.floor(Math.random() * 10000)
-    Log.BrightBlue(`[AssignID.js] Assigned Bet ID: ${id}`)
-    return id
+	const id = Math.floor(Math.random() * 10000)
+	return id
 }
 
 export async function assignMatchID() {
-    const matchId = Math.floor(Math.random() * 10000)
-    Log.BrightBlue(`[AssignIDs.js] Assigned ID: ${matchId}`)
-    return matchId
+	const matchId = Math.floor(Math.random() * 10000)
+	return matchId
 }
