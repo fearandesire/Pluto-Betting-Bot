ac96785297f5c1bb88f79102cccd0900b1d90648
fearandesire <63979371+fearandesire@users.noreply.github.com>
2023-12-07 14:00:17 -0500

    feat: :sparkles: Balance method
    Add method to get user balance into `AccountManager` to centralize code

--- utils/bot_res/classes/AccountManager.js
+++ utils/bot_res/classes/AccountManager.js
@@ -1,9 +1,15 @@
-import { db } from '#db';
-
+import { db } from '#db'
 
 export default class AccountManager {
 	constructor(accountTable) {
 		this.accountTable = accountTable
+	}
+
+	async getBalance(userId) {
+		return db.oneOrNone(
+			`SELECT balance FROM "${this.accountTable}" WHERE userid = $1`,
+			[userId],
+		)
 	}
 
 	async getAll() {
