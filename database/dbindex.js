27a24ef1dc56ed40ebf36685362ecb80efc9c64a
fearandesire <63979371+fearandesire@users.noreply.github.com>
2023-12-19 23:48:04 -0500

    refactor: :recycle: Load Redis config from DB

---
+++ database/dbindex.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-undef */
+
+import 'dotenv/config'
+
+import monitor from 'pg-monitor'
+import pgPromise from 'pg-promise'
+import { packageDirectory } from 'pkg-dir'
+
+const rootDir = await packageDirectory()
+const initOptions = {
+	// options for PG promise using initPromise
+	connect: true,
+	disconnect: true,
+	query: true,
+	error: true,
+	task: true,
+	transact: true,
+}
+
+const pgp = pgPromise(initOptions) // initialises options
+
+//* Logging pg-promise events with pg-monitor */
+
+monitor.attach(initOptions) // monitor to log
+
+const { SQLStr } = process.env
+
+//* PG PROMISE SETUP »»»»» */
+
+const sslrootcert = `${rootDir}/ca-certificate.crt`
+
+const cnString = `${SQLStr}sslrootcert=${sslrootcert}`
+
+export const db = pgp({
+	connectionString: cnString,
+	idleTimeoutMillis: 60000,
+	max: 4,
+})
--- utils/cache/redisInstance.js
+++ utils/cache/redisInstance.js
@@ -1,24 +1,25 @@
 import Redis from 'ioredis'
 import { Log } from '#config'
+import { R_HOST, R_PORT, R_PASS, R_DB } from '#serverConf'
 
 Log.Yellow(
 	`Connecting to Redis: ${process.env.R_HOST}:${process.env.R_PORT}`,
 )
 
 const redisCache = new Redis({
-	host: process.env.R_HOST,
-	port: parseInt(process.env.R_PORT, 10),
-	password: process.env.R_PASS,
+	host: R_HOST,
+	port: Number(R_PORT),
+	password: R_PASS,
 	connectTimeout: 10000, // Increase connection timeout to 10 seconds
 	retryStrategy: (times) =>
 		// Reconnect after a delay that increases with each failed attempt
 		Math.min(times * 50, 2000),
-	db: process.env.R_DB,
+	db: R_DB,
 })
 
 redisCache.on('connect', () => {
 	Log.Green(
-		`[REDIS] Connected to Redis server\nDB Selection: ${process.env.R_DB}`,
+		`[REDIS] Connected to Redis server\nDB Selection: ${R_DB}`,
 	)
 })
 
