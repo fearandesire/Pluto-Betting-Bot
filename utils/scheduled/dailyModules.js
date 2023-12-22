bdbeeab29641776ef1ece8adc7bb289ac42e7303
fearandesire <63979371+fearandesire@users.noreply.github.com>
2023-12-22 04:38:18 -0500

    refactor: :recycle: Rm unused score heartbeat

--- utils/scheduled/dailyModules.js
+++ utils/scheduled/dailyModules.js
@@ -4,7 +4,7 @@ import {
 	init_Cron_Completed,
 	init_Cron_Chan_Scheduler,
 	initMatchupHandling,
-	init_Cron_Heartbeat,
+	// init_Cron_Heartbeat,
 	queueMidnightCheck,
 } from './scheduledModules.js'
 import cronScheduleGames from '../db/gameSchedule/cronScheduleGames.js'
@@ -39,7 +39,7 @@ export async function dbDailyOps() {
 			await init_Cron_Completed(true), // Start range generation on-startup [Instant]
 			await queueMidnightCheck(), // Cron for checking games @ Midnight - 2 AM
 			await initMatchupHandling(), // Start Cron to generate Cron Ranges & Check for completed games [Daily]
-			await init_Cron_Heartbeat(), // Start Cron for Heartbeats
+			// await init_Cron_Heartbeat(), // Start Cron for Heartbeats
 		])
 	} catch (err) {
 		Log.Red(err)
