229473c298033b2060294b87c136e0c40dafa0a3
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-01-14 00:49:34 -0500

    refactor(pluto-api): :recycle: Rm unused endpoints
    We will be more of a receiving API for the Pluto logic from the new API
    Refactoring the main index of our API file for this purpose

--- utils/api/index.js
+++ utils/api/index.js
@@ -3,15 +3,11 @@ import cors from '@koa/cors'
 import logger from 'koa-logger'
 import bodyParser from 'koa-bodyparser'
 import logClr from '@pluto-internal-color-logger'
-import oddsRouter from '@pluto-iso-manager/odds.js'
-import checkGamesRouter from '@pluto-iso-manager/checkGames.js'
-import fetchBetsRouter from '@pluto-iso-manager/fetchBets.js'
-import fetchAccountsRouter from '@pluto-iso-manager/fetchAccounts.js'
-import fetchScheduledRouter from '@pluto-iso-manager/fetchScheduledChannels.js'
 import {
 	pageNotFound,
 	responseTime,
 } from './requests/middleware.js'
+import incomingChannelsRouter from './routes/channels/incoming.js'
 
 const app = new Koa()
 app.use(logger())
@@ -19,20 +15,9 @@ app.use(cors())
 app.use(bodyParser())
 app.use(pageNotFound)
 app.use(responseTime)
-app.use(oddsRouter.routes()).use(
-	oddsRouter.allowedMethods(),
-)
-app.use(checkGamesRouter.routes()).use(
-	checkGamesRouter.allowedMethods(),
-)
-app.use(fetchBetsRouter.routes()).use(
-	fetchBetsRouter.allowedMethods(),
-)
-app.use(fetchAccountsRouter.routes()).use(
-	fetchAccountsRouter.allowedMethods(),
-)
-app.use(fetchScheduledRouter.routes()).use(
-	fetchScheduledRouter.allowedMethods(),
+
+app.use(incomingChannelsRouter.routes()).use(
+	incomingChannelsRouter.allowedMethods(),
 )
 
 const { apiPort, apiURL } = process.env
