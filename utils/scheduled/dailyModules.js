6fd77605ce0bdc908e74538819b29f27191d224b
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-01-16 10:12:45 -0500

    ♻️ Refactor dailyModule and scheduledModule to remove unused code

--- utils/scheduled/dailyModules.js
+++ utils/scheduled/dailyModules.js
@@ -1,20 +1,16 @@
 import { Log } from '@pluto-core-config'
 import logClr from '@pluto-internal-color-logger'
-import {
-	init_Cron_Completed,
-	init_Cron_Chan_Scheduler,
-	initMatchupHandling,
-	// init_Cron_Heartbeat,
-	queueMidnightCheck,
-} from './scheduledModules.js'
-import collectOdds from '../bot_res/betOps/collectOdds.js'
+import // init_Cron_Completed,
+// initMatchupHandling,
+// init_Cron_Heartbeat,
+// queueMidnightCheck,
+'./scheduledModules.js'
 import { clearPendingBets } from './dailyModules_Utils.js'
 import { handleBetMatchups } from '../bot_res/betOps/handleBetMatchups.js'
-import GameScheduler from '../db/gameSchedule/GameScheduler.js'
 
 /**
  * @function dbDailyOps
- * Functions executed daily
+ * Functions executed daily / Init/Startup Operations
  * Includes:
  *   Clear pending bets and prior scheduled games
  * - Cron jobs for checking completed games & score
@@ -30,13 +26,20 @@ export async function dbDailyOps() {
 	try {
 		await Promise.all([
 			await handleBetMatchups(),
+			Log.Yellow(`Completed handleBetMatchups`),
 			await clearPendingBets(), // Clear Pending Bets - In this context, bets that have not been confirmed or cancelled.
-			await collectOdds(), // Collect Odds on-start [Instant]
-			await new GameScheduler().init(), // Check for any games that need to be scheduled now (Game Channels) [Instant]
-			await init_Cron_Chan_Scheduler(), // Start Cron to schedule games daily (Game Channels) [Daily]
-			await init_Cron_Completed(true), // Start range generation on-startup [Instant]
-			await queueMidnightCheck(), // Cron for checking games @ Midnight - 2 AM
-			await initMatchupHandling(), // Start Cron to generate Cron Ranges & Check for completed games [Daily]
+			Log.Yellow(`Completed clearPendingBets`),
+			//			await collectOdds(), // Collect Odds on-start [Instant]
+			Log.Yellow(`Completed collectOdds`),
+			// await new ChannelManager(
+			// 	server_ID,
+			// ).syncScheduled(),
+			Log.Yellow(`Completed syncScheduled`),
+			// await new GameScheduler().init(), // Check for any games that need to be scheduled now (Game Channels) [Instant]
+			// await init_Cron_Chan_Scheduler(), // Start Cron to schedule games daily (Game Channels) [Daily]
+			// await queueMidnightCheck(), // Cron for checking games @ Midnight - 2 AM
+			// await initMatchupHandling(), // Start Cron to generate Cron Ranges & Check for completed games [Daily]
+			// Log.Green(`Completed init modules`),
 			// await init_Cron_Heartbeat(), // Start Cron for Heartbeats
 		])
 	} catch (err) {
