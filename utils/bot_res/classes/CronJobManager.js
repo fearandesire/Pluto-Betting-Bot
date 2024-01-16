9f7a8c171c805f8ebde622718d60df80f8643b84
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-01-16 10:13:21 -0500

    ♻️ (CronJobManager.js): simplify class and remove cache related functions

--- utils/bot_res/classes/CronJobManager.js
+++ utils/bot_res/classes/CronJobManager.js
@@ -2,44 +2,15 @@ import { schedule } from 'node-cron'
 import { Log } from '@pluto-internal-logger'
 
 export default class CronJobManager {
-	constructor(cacheManager) {
-		this.cacheManager = cacheManager
-		this.keepAliveInterval = 300 // Time in seconds to reset the TTL
-	}
-
 	async scheduleJob(id, cronExpression, task) {
 		const jobExists = await this.cacheManager().get(id)
 		if (jobExists) {
 			Log.Yellow(
-				`Job with ID ${id} already scheduled. Skipping...`,
+				`Unable to add to schedule: Job with ID ${id} already scheduled. .`,
 			)
 			return
 		}
 
 		await schedule(cronExpression, task)
-		const jobData = { id, cronExpression } // Store only the necessary details
-		await this.cacheManager().set(
-			id,
-			jobData,
-			this.keepAliveInterval,
-		)
-		await this.startJobKeepAlive(id)
-		return Log.Blue(`Scheduled new job with ID ${id}`)
 	}
-
-	async resetJobTTL(id) {
-		const job = await this.cacheManager().get(id)
-		if (job) {
-			await this.cacheManager().set(id, job)
-		}
-	}
-
-	startJobKeepAlive(id) {
-		setInterval(async () => {
-			Log.Blue(`Resetting TTL for job ID: ${id}`)
-			await this.resetJobTTL(id)
-		}, this.keepAliveInterval * 1000) // Convert seconds to milliseconds
-	}
-
-	// Additional methods for job management (e.g., cancelJob, rescheduleJob) can be added here.
 }
