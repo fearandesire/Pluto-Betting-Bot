fb612cecb7b229e8e28db3c95ca071773494cc5d
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-01-16 00:54:26 -0500

    ✨ (AuthManager.js): Added methods to check token validity and update tokens   🔧 (AuthManager.js): Use environment variable for 'admin-token' to increase security   🚀 (AuthManager.js): Improve request url constructions through the usage of api endpoints map (for example `this.ep.auth`) to increase readability and maintainability   ♻️ (AuthManager.js): Refactor username input for requests to use imported `pluto_api_username` to centralize configuration changes.

--- utils/api/auth/AuthManager.js
+++ utils/api/auth/AuthManager.js
@@ -1,21 +1,63 @@
 import axios from 'axios'
-import { admin_token, pluto_api_url, pluto_api_username } from "../../serverConfig.js"
+import {
+	admin_token,
+	pluto_api_url,
+	pluto_api_username,
+} from '../../serverConfig.js'
 
 export default class AuthManager {
 	constructor() {
-		this.API_URL = pluto_api_url
 		this.ep = {
 			channels: `game-channel`,
 			auth: `auth`,
+			validity: 'auth/valid',
+			refresh: `auth/refresh`,
+			gToken: `auth/stored-token`,
+		}
+
+		this.API_URL = pluto_api_url
+	}
+
+	async updateToken(token) {
+		try {
+			await axios.post(
+				`${this.API_URL}/${this.ep.auth}`,
+				{
+					username: pluto_api_username,
+					token,
+				},
+			)
+		} catch (err) {
+			console.error(err)
+			throw err
+		}
+	}
+
+	async isTokenExpired(token) {
+		try {
+			await axios.get(
+				`${this.API_URL}/${this.ep.validity}`,
+				{
+					body: {
+						token,
+					},
+				},
+			)
+			return false
+		} catch (err) {
+			if (err.response.status === 401) {
+				return true
+			}
+			throw err
 		}
 	}
 
 	async getToken() {
 		try {
 			const tokenReq = await axios.post(
-				`${pluto_api_url}/auth/stored-token`,
+				`${this.API_URL}/${this.ep.gToken}`,
 				{
-					username: `fenix`,
+					username: pluto_api_username,
 				},
 				{
 					headers: {
@@ -33,14 +75,13 @@ export default class AuthManager {
 	async refreshToken() {
 		try {
 			const tokenReq = await axios.post(
-				`${this.API_URL}/${this.ep.auth}/refresh`,
+				`${this.API_URL}/${this.ep.refresh}`,
 				{
-					username:
-					pluto_api_username,
+					username: pluto_api_username,
 				},
 				{
 					headers: {
-						'admin-token': `${admin_token}`,
+						'admin-token': `${process.env.PLUTO_API_TOKEN}`,
 					},
 				},
 			)
@@ -51,4 +92,3 @@ export default class AuthManager {
 		}
 	}
 }
-
