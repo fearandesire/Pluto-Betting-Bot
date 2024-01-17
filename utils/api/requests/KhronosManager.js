4367bba572896d55679551e15359219718c58e71
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-01-17 16:09:26 -0500

    ✨ Create KhronosManager.js and integrate Pluto API for game schedule and categories fetching - This introduces a new API manager for communicating with the Pluto API. - The manager provides methods to fetch game schedules and categories, which will be utilized in other parts of the application.

---
+++ utils/api/requests/KhronosManager.js
@@ -0,0 +1,91 @@
+import axios from 'axios'
+import { pluto_api_url } from '../../serverConfig.js'
+
+export default class KhronosManager {
+	constructor() {
+		this.url = `${pluto_api_url}`
+		this.epts = {
+			game_schedule: 'discord/config/type',
+			categories: {
+				by_sport: 'discord/configs/games',
+				all: 'discord/configs/games/all',
+			},
+		}
+	}
+
+	/**
+	 * Fetch all Game Schedule Channel IDs Pluto is serving
+	 * @returns @ConfigType
+	 */
+	async fetchGameScheduleChannels() {
+		const channelsData = await this.fetchByType(
+			`DAILY_SCHEDULE_CHAN`,
+		)
+		return channelsData
+	}
+
+	async fetchByType(type) {
+		try {
+			const response = await axios.get(
+				`${pluto_api_url}/${this.epts.game_schedule}`,
+				{
+					params: {
+						type,
+					},
+					headers: {
+						'admin-token': `${process.env.PLUTO_API_TOKEN}`,
+					},
+				},
+			)
+			return response.data
+		} catch (err) {
+			console.error(err)
+			return false
+		}
+	}
+
+	/**
+	 * Fetch Categories Pluto is serving for a specific sport
+	 * @returns @ConfigType
+	 */
+	async fetchGameCategoriesBySport(sport) {
+		try {
+			const response = await axios.get(
+				`${pluto_api_url}/${this.epts.categories.by_sport}`,
+				{
+					params: {
+						sport: `${sport.toUpperCase()}`,
+					},
+					headers: {
+						'admin-token': `${process.env.PLUTO_API_TOKEN}`,
+					},
+				},
+			)
+			return response.data
+		} catch (err) {
+			console.error(err)
+			return false
+		}
+	}
+
+	/**
+	 * Fetch All Game Channel Category IDs Pluto is serving
+	 * @returns @ConfigType
+	 */
+	async fetchGameCategories() {
+		try {
+			const response = await axios.get(
+				`${pluto_api_url}/${this.epts.categories.all}`,
+				{
+					headers: {
+						'admin-token': `${process.env.PLUTO_API_TOKEN}`,
+					},
+				},
+			)
+			return response.data
+		} catch (err) {
+			console.error(err)
+			return false
+		}
+	}
+}
