5e51c8dd86ab01d987168189094fe2859e999dec
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-01-17 16:10:18 -0500

    ✨ (GameSchedule.js): Added createScheduleEmbed, sendDailyGames, and parseAndFormat methods to handle scheduling of games feature   💡 Documented and commented methods to improve code readability   ♻️ Refactored formatForSchedule method to improve team name enumeration

--- utils/api/requests/matchups/GameSchedule.js
+++ utils/api/requests/matchups/GameSchedule.js
@@ -1,14 +1,88 @@
 import axios from 'axios'
+import { AttachmentBuilder, EmbedBuilder } from 'discord.js'
 import { pluto_api_url } from '../../../serverConfig.js'
 import { findEmoji } from '../../../bot_res/findEmoji.js'
+import embedColors from '../../../../lib/colorsConfig.js'
+import GuiltUtils from '../../utils/GuildUtils.js'
 
+/**
+ * Responsible for retrieving & displaying upcoming games / matchups
+ */
 export default class GameSchedule {
+	async createScheduleEmbed(desc) {
+		// Make today's date string: `DD/MM/YYYY`
+		const date = new Date()
+		const today = date.toLocaleDateString('en-US', {
+			month: '2-digit',
+			day: '2-digit',
+			year: 'numeric',
+		})
+		const scheduleEmbed = new EmbedBuilder()
+			.setDescription(
+				`## Daily Schedule | ${today}\n${desc}`,
+			)
+			.setColor(embedColors.PlutoRed)
+			.setFooter({ text: `dev. fenixforever` })
+		return { scheduleEmbed }
+	}
+
+	/**
+	 * Formats and sends the games for the current day
+	 */
+	async sendDailyGames(sport, games, rows) {
+		// Fetch the schedule, format it
+		const gamesStr = await this.parseAndFormat(games)
+		const { scheduleEmbed } =
+			await this.createScheduleEmbed(gamesStr)
+		// Send the schedule to every daily_schedule channel
+		for (const row of rows) {
+			const chanId = row.setting_value
+			const guildId = row.guild_id
+			try {
+				const chan =
+					await new GuiltUtils().getChanViaGuild({
+						guildId,
+						chanId,
+					})
+				await chan.send({ embeds: [scheduleEmbed] })
+			} catch (err) {
+				console.error(err)
+				continue
+			}
+		}
+	}
+
+	async parseAndFormat(games) {
+		let scheduleStr = ''
+		for (const game of games) {
+			const formattedGame =
+				await this.formatForSchedule(game)
+			scheduleStr += `${formattedGame}\n` // Append each game's string
+
+			// Check Discord character limit
+			if (scheduleStr.length > 1950) {
+				// Keeping some buffer
+				scheduleStr += '... and more games'
+				break
+			}
+		}
+
+		return scheduleStr
+	}
+
 	/**
 	 * Query Khronos API
 	 * Get games for today
+	 * Parse & Format into a conjoined string detailing the matchups schedule for today
+	 * @returns {string}
 	 */
 	async getFormattedSchedule(sport) {
-		const games = await this.fetchViaSport(sport)
+		let games
+		if (!sport) {
+			games = await this.reqAll() // Fetches schedule
+		} else {
+			games = await this.fetchViaSport(sport)
+		}
 		let scheduleStr = ''
 
 		for (const game of games) {
@@ -69,24 +143,20 @@ export default class GameSchedule {
 	```
 	 */
 	async formatForSchedule(game) {
-		// Shorten team names (assuming they're in the format "City TeamName")
-		const homeTeamShort = game.home_team
-			.split(' ')
-			.pop()
-		const awayTeamShort = game.away_team
-			.split(' ')
-			.pop()
-		const homeTeamEmoji =
-			(await findEmoji(game.home_team)) || ''
-		const awayTeamEmoji =
-			(await findEmoji(game.away_team)) || ''
+		const [homeTeamShort, awayTeamShort] = [
+			game.home_team,
+			game.away_team,
+		].map((name) => name.split(' ').pop())
+		const [homeTeamEmoji, awayTeamEmoji] =
+			await Promise.all([
+				GuiltUtils.findEmoji(game.home_team),
+				GuiltUtils.findEmoji(game.away_team),
+			])
+
 		const unixTimestamp = Math.floor(
 			new Date(game.commence_time).getTime() / 1000,
 		)
-
-		// Format game string with bold team names and italic records
-		const gameStr = `**${awayTeamEmoji} ${awayTeamShort} *(${game.teamRecords[1]})*** at **${homeTeamEmoji} ${homeTeamShort} *(${game.teamRecords[0]})*** @ *<t:${unixTimestamp}:t>*`
-		return gameStr
+		return `**${awayTeamEmoji} ${awayTeamShort} *(${game.teamRecords[1]})*** *\`at\`* **${homeTeamEmoji} ${homeTeamShort} *(${game.teamRecords[0]})*** @ *<t:${unixTimestamp}:t>*`
 	}
 
 	async reqAll() {
