a78988271ebca57db2bb8946a53f3ea0f5578c57
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-01-18 22:27:37 -0500

    ✨ Add new interface file (ApiInterfaces.ts) for defining sports serving enums and type definitions for configuration and matchup-related data

---
+++ lib/interfaces/api/ApiInterfaces.ts
@@ -0,0 +1,39 @@
+export enum SportsServing {
+	NBA = 'NBA',
+	NFL = 'NFL',
+}
+
+export type KH_ValidConfigType =
+	| `BETTING_CHAN`
+	| `LOGS`
+	| `GAMES_CATEGORY`
+	| `SECURITY`
+	| 'DAILY_SCHEDULE_CHAN'
+
+// For: Daily Schedule
+export interface IMatchupAggregated {
+	id: string
+	sport_title: string
+	commence_time: string
+	home_team: string
+	away_team: string
+	last_update: string
+	home_team_odds: number
+	away_team_odds: number
+	winner: string | null
+	loser: string | null
+	dateofmatchup: string
+	legiblestart: string
+	cron_timer: string
+	closing_bets: boolean
+	broadcastInfo: string[]
+	teamRecords: string[]
+}
+
+export interface IConfigRow {
+	rowid: number
+	guild_id: string
+	setting_type: string
+	setting_value: string
+	sport: string
+}
