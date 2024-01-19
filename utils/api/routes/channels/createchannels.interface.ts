b1b4423840dc07b32f35da19272ba8d32f71c98a
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-01-18 22:26:45 -0500

    ✨ Add new types IChannelAPI and IChannelAggregatedAPI in createchannels.interface.ts for better type safety in channel API

---
+++ utils/api/routes/channels/createchannels.interface.ts
@@ -0,0 +1,26 @@
+import { SportsServing } from 'lib/interfaces/api/ApiInterfaces'
+
+export interface IChannelAPI {
+	id: string
+	sport: string
+	channelname: string
+	crontime: string
+	gametime: string
+	created: boolean
+}
+
+export interface IChannelAggregatedAPI {
+	id: string
+	sport: SportsServing
+	channelname: string
+	crontime: string
+	gametime: string
+	created: boolean
+	matchupOdds: {
+		favored: string
+		home_team_odds: number
+		away_team_odds: number
+	}
+	home_team: string
+	away_team: string
+}
