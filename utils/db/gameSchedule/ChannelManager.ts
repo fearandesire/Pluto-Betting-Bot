152c3b6c509ceb1c3b5a4d479461b4ce758e7ae7
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-01-18 22:26:34 -0500

    ✨ (ChannelManager.ts): Convert from JavaScript to TypeScript. ♻️ (ChannelManager.ts): Refactor to use TypeScript types and interfaces for method parameters and returns. 🐛 (ChannelManager.ts): Fix names to match TypeScript conventions. ⬆️ (ChannelManager.ts): Change variable 'teamResolver' to 'resolveTeam' to reflect imported module name. 💡 (ChannelManager.ts): Remove unused, commented methods for cleaner code. 🔧 (ChannelManager.ts): Upgrade eslint linting rules in comments.

--- utils/db/gameSchedule/ChannelManager.ts
+++ utils/db/gameSchedule/ChannelManager.ts
@@ -4,21 +4,41 @@ import PlutoLogger from '@pluto-logger'
 import { Log } from '@pluto-internal-logger'
 import { addMinutes, format } from 'date-fns'
 import cron from 'node-cron'
-import teamResolver from 'resolve-team'
+import resolveTeam from 'resolve-team'
 import _ from 'lodash'
-import discord, { EmbedBuilder } from 'discord.js'
+import discord, {
+	CategoryChannelResolvable,
+	ColorResolvable,
+	EmbedBuilder,
+} from 'discord.js'
 import AuthManager from '../../api/auth/AuthManager.js'
 import { pluto_api_url } from '../../serverConfig.js'
 import { getCategories } from '../../api/utils/getCategories.js'
 import { findEmoji } from '../../bot_res/findEmoji.js'
+import { IChannelAggregatedAPI } from 'utils/api/routes/channels/createchannels.interface.js'
+import { IConfigRow } from 'lib/interfaces/api/ApiInterfaces.js'
 
 const { ChannelType } = discord
 
+interface IPrepareMatchEmbed {
+	favored: string
+	favoredTeamClr: ColorResolvable
+	home_team: string
+	away_team: string
+	bettingChanId: string
+}
 /**
  * Handle interactions between Pluto API & Discord user interface/interactions
  */
 export default class ChannelManager {
-	constructor(guildId) {
+	private serverId: string
+	private API_URL: string
+	private ep: {
+		gchan: string
+	}
+	private authManager: AuthManager
+
+	constructor(guildId: string) {
 		this.serverId = guildId
 		this.API_URL = `${pluto_api_url}`
 		this.ep = {
@@ -33,20 +53,22 @@ export default class ChannelManager {
 	 * @param {Object} channel - The channel data to process.
 	 * @param {Array} bettingChanRows - Array of betting channel data.
 	 */
-	async processChannel(channel, bettingChanRows) {
+	async processChannel(
+		channel: IChannelAggregatedAPI,
+		bettingChanRows: IConfigRow[],
+	) {
 		const { matchupOdds } = channel
 		const { favored } = matchupOdds
-		const favoredTeamInfo = await teamResolver(
+		const favoredTeamInfo = await resolveTeam(
 			channel.sport.toLowerCase(),
 			favored,
 			{ full: true },
 		)
 		this.validateFavoredTeamInfo(favoredTeamInfo)
-		const categoriesData = await getCategories()
-		if (!categoriesData)
-			throw new Error(`Could not get categories.`)
+		const gameCategories = await getCategories()
+		if (!gameCategories) throw new Error(`Could not get categories.`)
 
-		for (const category of categoriesData) {
+		for (const category of gameCategories) {
 			await this.createChannelAndSendEmbed(
 				channel,
 				category,
@@ -57,64 +79,13 @@ export default class ChannelManager {
 	}
 
 	/**
-	 * Schedules channels based on the data received from the API.
-	 */
-	async scheduleChannelsFromAPI() {
-		try {
-			const channelData = (await this.reqChannels())
-				.data
-			if (!Array.isArray(channelData)) {
-				console.error('Invalid channel data format')
-				return false
-			}
-
-			for (const channel of channelData) {
-				const args = {
-					scheduledCreationTime: channel.crontime,
-					chanName: channel.channelname,
-					createNow: false, // Set to false by default
-				}
-
-				const { awayTeam, homeTeam } =
-					this.sortHomeVsAway(args.chanName)
-				const success = await this.scheduleChannels(
-					homeTeam,
-					awayTeam,
-					args,
-				)
-				if (!success) {
-					console.error(
-						`Failed to schedule channel: ${args.chanName}`,
-					)
-				}
-			}
-
-			console.log(
-				'All channels scheduled successfully',
-			)
-			return true
-		} catch (error) {
-			console.error(
-				'Error in scheduling channels from API:',
-				error,
-			)
-			return false
-		}
-	}
-
-	/**
 	 * Validates the resolved team information.
 	 * @param {Object} favoredTeamInfo - The resolved team data.
 	 * @throws {Error} If team colors or data are unavailable.
 	 */
-	validateFavoredTeamInfo(favoredTeamInfo) {
-		if (
-			!favoredTeamInfo ||
-			_.isEmpty(favoredTeamInfo.colors)
-		) {
-			throw new Error(
-				'Unable to resolve team colors or data',
-			)
+	validateFavoredTeamInfo(favoredTeamInfo: any) {
+		if (!favoredTeamInfo || _.isEmpty(favoredTeamInfo.colors)) {
+			throw new Error('Unable to resolve team colors or data')
 		}
 	}
 
@@ -122,21 +93,22 @@ export default class ChannelManager {
 	 * Creates a channel and sends an embed message to it.
 	 * @async
 	 * @param {Object} channel - Channel data.
-	 * @param {Object} category - Category data.
+	 * @param {Object} configRow - Category data.
 	 * @param {Array} bettingChanRows - Array of betting channel data.
 	 * @param {Object} favoredTeamInfo - Resolved team information.
 	 */
 	async createChannelAndSendEmbed(
-		channel,
-		category,
-		bettingChanRows,
-		favoredTeamInfo,
+		channel: IChannelAggregatedAPI,
+		configRow: IConfigRow,
+		bettingChanRows: IConfigRow[],
+		favoredTeamInfo: any,
 	) {
-		const guild = SapDiscClient.guilds.cache.get(
-			category.guild_id,
-		)
+		const guild = SapDiscClient.guilds.cache.get(configRow.guild_id)
+
+		if (!guild) return null
+
 		const guildsCategory = guild.channels.cache.get(
-			`${category.setting_value}`,
+			`${configRow.setting_value}`,
 		)
 		const sortedBetChan = bettingChanRows.find(
 			(row) => row.guild_id === guild.id,
@@ -145,53 +117,46 @@ export default class ChannelManager {
 		const bettingChanId = sortedBetChan?.setting_value
 		const { home_team, away_team } = channel
 		if (_.isEmpty(home_team) || _.isEmpty(away_team)) {
-			throw new Error(
-				`Missing home and away teams in channel data.`,
-			)
+			throw new Error(`Missing home and away teams in channel data.`)
+		}
+		if (!bettingChanId) {
+			throw new Error(`Missing betting channel id in channel data.`)
 		}
 
 		const { matchupOdds } = channel
-
-		const matchEmbed = await this.prepareMatchupEmbed({
+		const args = {
 			favored: matchupOdds.favored,
 			favoredTeamClr: favoredTeamInfo.colors[0],
 			home_team,
 			away_team,
 			bettingChanId,
-		})
+		}
+		const matchEmbed = await this.prepareMatchupEmbed(args)
 
 		const gameChan = await guild.channels.create({
 			name: `${channel.channelname}`,
 			type: ChannelType.GuildText,
 			topic: `Enjoy the Game!`,
-			parent: guildsCategory,
+			parent: guildsCategory as CategoryChannelResolvable,
 		})
 
 		await gameChan.send({ embeds: [matchEmbed.embed] })
-		console.log(
-			`Created channel: ${channel.channelname} `,
-		)
+		console.log(`Created channel: ${channel.channelname} `)
 	}
 
-	async prepareMatchupEmbed({
-		favored,
-		favoredTeamClr,
-		home_team,
-		away_team,
-		bettingChanId,
-	}) {
-		const embedClr = favoredTeamClr
-		const teamEmoji = (await findEmoji(favored)) || ''
-		const title = `${away_team} @ ${home_team}`
+	async prepareMatchupEmbed(args: IPrepareMatchEmbed) {
+		const embedClr = args.favoredTeamClr
+		const teamEmoji = (await findEmoji(args.favored)) || ''
+		const title = `${args.away_team} @ ${args.home_team}`
 
 		const matchEmbed = new EmbedBuilder()
 			.setColor(embedClr)
 			.setTitle(title)
 			.setDescription(
 				`
-**The ${teamEmoji} ${favored} are favored to win this game!**
+**The ${teamEmoji} ${args.favored} are favored to win this game!**
 
-*Type \`/commands\` in the <#${bettingChanId}> channel to place bets with Pluto*`,
+*Type \`/commands\` in the <#${args.bettingChanId}> channel to place bets with Pluto*`,
 			)
 			.setFooter({
 				text: `Pluto | Created by fenixforever`,
@@ -200,19 +165,15 @@ export default class ChannelManager {
 		return { embed: matchEmbed }
 	}
 
-	async locateChannel(serverid, channelName) {
+	async locateChannel(serverid: string, channelName: string) {
 		// Find channel in guild
-		const guild = await SapDiscClient.guilds.cache.get(
-			`${serverid}`,
-		)
+		const guild = await SapDiscClient.guilds.cache.get(`${serverid}`)
 
 		if (!guild) {
 			return false
 		}
 		const channel = await guild.channels.cache.find(
-			(GC) =>
-				GC.name.toLowerCase() ===
-				channelName.toLowerCase(),
+			(GC) => GC.name.toLowerCase() === channelName.toLowerCase(),
 		)
 		if (!channel) {
 			return false
@@ -227,20 +188,13 @@ export default class ChannelManager {
 	 * @returns {object} - The channel object, or false if not found
 	 */
 
-	async deleteChan(channelName) {
+	async deleteChan(channelName: string) {
 		// Replace spaces with -
-		const parsedChanName = channelName.replace(
-			/\s/g,
-			'-',
-		)
-		const gameChan = await this.locateChannel(
-			this.serverId,
-			channelName,
-		)
+		const parsedChanName = channelName.replace(/\s/g, '-')
+		const gameChan = await this.locateChannel(this.serverId, channelName)
 		if (!gameChan) {
 			await Log.Red(
 				`Unable to locate channel \`${parsedChanName}\` to delete.`,
-				`#ff0000`,
 			)
 			return false
 		}
@@ -254,13 +208,11 @@ export default class ChannelManager {
 	 * Removes game from the Pluto Channel API
 	 * @param {string} gameChanName The name of the channel to delete
 	 */
-	async queueDeleteChannel(gameChanName, id) {
+	async queueDeleteChannel(gameChanName: string, id: string) {
 		const rn = new Date()
 		const currMin = addMinutes(rn, 5) // format current time + x minutes
 		const newMinRaw = format(currMin, 's mm H d M i')
 		const splitTime = newMinRaw.split(' ')
-		// eslint-disable-next-line no-unused-vars
-		const secs = splitTime[0]
 		const mins = splitTime[1]
 		const hours = splitTime[2]
 		const day = splitTime[3]
@@ -273,23 +225,21 @@ export default class ChannelManager {
 		})
 		cron.schedule(cronString, async () => {
 			try {
-				await this.deleteChan(gameChanName).then(
-					async (res) => {
-						if (res) {
-							await PlutoLogger.log({
-								id: 2,
-								description: `Deleted game channel ${gameChanName}`,
-							})
-							return true
-						}
+				await this.deleteChan(gameChanName).then(async (res) => {
+					if (res) {
 						await PlutoLogger.log({
 							id: 2,
-							description: `Failed to delete game channel ${gameChanName}\nThis channel was likely already deleted.`,
+							description: `Deleted game channel ${gameChanName}`,
 						})
-						return false
-					},
-				)
-			} catch (error) {
+						return true
+					}
+					await PlutoLogger.log({
+						id: 2,
+						description: `Failed to delete game channel ${gameChanName}\nThis channel was likely already deleted.`,
+					})
+					return false
+				})
+			} catch (error: any) {
 				await PlutoLogger.log({
 					id: 2,
 					description: `Failed to delete game channel ${gameChanName}\nError: ${error.message}`,
@@ -302,46 +252,11 @@ export default class ChannelManager {
 		await console.log(`Removed ${id} from Khronos API`)
 	}
 
-	async deleteFromAPI(id) {
-		return axios.delete(
-			`${this.API_URL}/${this.ep.gchan}/${id}`,
-			{
-				headers: {
-					'admin-token': `${process.env.PLUTO_API_TOKEN}`,
-				},
+	async deleteFromAPI(id: string) {
+		return axios.delete(`${this.API_URL}/${this.ep.gchan}/${id}`, {
+			headers: {
+				'admin-token': `${process.env.PLUTO_API_TOKEN}`,
 			},
-		)
-	}
-
-	/**
-	 * Fetches the scheduled channels.
-	 *
-	 * @return {Promise<Array>} An array of channel data.
-	 */
-	async fetchScheduledChannels() {
-		try {
-			const channelsData = await this.reqChannels()
-			return channelsData
-		} catch (err) {
-			console.error(err)
-			return false
-		}
-	}
-
-	async reqChannels() {
-		const currentToken =
-			await new AuthManager().getToken()
-		const channelsQuery = axios.get(
-			`${this.API_URL}/game-channel/all`,
-			{
-				params: {
-					serverid: this.serverId,
-				},
-				headers: {
-					Authorization: `Bearer ${currentToken}`,
-				},
-			},
-		)
-		return channelsQuery.data
+		})
 	}
 }
