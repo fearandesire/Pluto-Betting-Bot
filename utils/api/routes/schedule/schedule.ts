23d68605b661fcd715b0005d9afb1c0016765acb
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-01-18 21:53:42 -0500

    ♻️ (schedule.js): migrate ScheduleRouter from JS to TypeScript (schedule.ts)   🐛 (schedule.ts): improve type safety by adding ScheduleRequestBody interface and other explicit type definitions   💡️ (schedule.ts): provide detailed type annotations for clearer code understanding   ✨ (schedule.ts): improve sports list tendency for easy expansion in the future

---
+++ utils/api/routes/schedule/schedule.ts
@@ -0,0 +1,77 @@
+import Router from 'koa-router'
+import _ from 'lodash'
+import { Log } from '@pluto-core-config'
+import GameSchedule from '../../requests/matchups/GameSchedule.js'
+import {
+	IConfigRow,
+	IMatchupAggregated,
+	sportsServing,
+} from 'lib/interfaces/api/ApiInterfaces.js'
+
+const ScheduleRouter = new Router()
+interface ScheduleRequestBody {
+	aggregatedMatchups: IMatchupAggregated[]
+	dailyScheduleRows: IConfigRow[]
+}
+ScheduleRouter.post('/schedule/daily/all', async (ctx) => {
+	try {
+		const requestBody: ScheduleRequestBody = ctx.request.body as
+			| ScheduleRequestBody
+			| any
+		const { aggregatedMatchups, dailyScheduleRows } =
+			await validateAndParseSchedule(requestBody)
+		const sports: sportsServing[] = ['NBA', 'NFL'] // Extendable to other sports
+		const gameSchedule = new GameSchedule()
+
+		for (const sport of sports) {
+			await Log.Yellow(`Processing Daily Schedule for ${sport}`)
+			const games = filterGames(aggregatedMatchups, sport)
+			const rows = filterRows(dailyScheduleRows, sport)
+
+			if (!_.isEmpty(games)) {
+				await gameSchedule.sendDailyGames(sport, games, rows)
+				await Log.Green(`Successfully sent daily schedule for ${sport}`)
+			}
+		}
+
+		ctx.body = {
+			message: 'Daily schedule successfully sent!',
+		}
+		ctx.status = 200
+	} catch (err) {
+		console.error(err)
+		ctx.body = err
+	}
+})
+
+function filterGames(games: IMatchupAggregated[], sport: sportsServing) {
+	return _.filter(games, (game) => game.sport_title === sport)
+}
+
+function filterRows(rows: IConfigRow[], sport: sportsServing) {
+	return _.filter(
+		rows,
+		(row) => typeof row.sport === 'string' && row.sport === sport,
+	)
+}
+
+async function validateAndParseSchedule(
+	body: ScheduleRequestBody,
+): Promise<ScheduleRequestBody> {
+	const { aggregatedMatchups, dailyScheduleRows } = body
+	await console.log(
+		`aggregatedMatchups`,
+		aggregatedMatchups,
+		`\n`,
+		`dailyScheduleRows`,
+		`\n`,
+		dailyScheduleRows,
+	)
+	if (_.isEmpty(aggregatedMatchups) || _.isEmpty(dailyScheduleRows)) {
+		throw new Error('Invalid or missing schedule data')
+	}
+
+	return { aggregatedMatchups, dailyScheduleRows }
+}
+
+export default ScheduleRouter
