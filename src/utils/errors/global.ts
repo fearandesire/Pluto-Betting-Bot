8166627536dfd0a1b3bc054d72a41a615782e846
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-02-11 19:09:30 -0500

    refactor: Implement Factory Pattern
    New:
    Add factory pattern to simplify gen. of error embeds

--- src/utils/errors/global.ts
+++ src/utils/errors/global.ts
@@ -1,17 +1,48 @@
 import { EmbedBuilder } from 'discord.js'
 import embedColors from '../../lib/colorsConfig.js'
+import { helpfooter } from '@pluto-core-config'
 
+/**
+ * @summary Generates Error Embeds
+ *
+ * Utilizing a factory pattern, this class will dynamically create 'error' embeds
+ */
 export class ErrorEmbeds {
-	static internalErr = (description: string) => {
+	// Generalized method for creating error embeds
+	static createErrorEmbed(
+		title: string,
+		description: string,
+		extraData?: { footerData?: string; [key: string]: any },
+	) {
 		return new EmbedBuilder()
-			.setTitle(`Internal Error`)
+			.setTitle(title)
 			.setDescription(description)
 			.setColor(embedColors.error)
+			.setFooter({
+				text: extraData
+					? extraData.footerData + ' | ' + helpfooter
+					: helpfooter,
+			})
 	}
-	static betErr = (description: string) => {
-		return new EmbedBuilder()
-			.setTitle(`Invalid Bet`)
-			.setDescription(description)
-			.setColor(embedColors.error)
+
+	// Specific error methods just call the generalized method with predefined titles
+	static internalErr(description: string) {
+		return this.createErrorEmbed('Internal Error', description)
+	}
+
+	static betErr(description: string) {
+		return this.createErrorEmbed('Invalid Bet', description)
+	}
+
+	static invalidRequest(description: string) {
+		return this.createErrorEmbed('Invalid Request', description)
+	}
+
+	static accountErr(description: string) {
+		return this.createErrorEmbed('Account Error', description)
+	}
+
+	static unknownErr(description: string) {
+		return this.createErrorEmbed('Unknown Error', description)
 	}
 }
