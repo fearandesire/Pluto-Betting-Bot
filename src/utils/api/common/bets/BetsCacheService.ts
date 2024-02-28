d9610bfc2ccc5aadeeb04ac43f2fbe24598297b3
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-02-28 02:06:13 -0500

    feat: Update typings; Add sanitization method
    - Integrate typings from Khronos
    - Add `sanitize` method to be used in the betting process
     - Method will structure a cached betslip into the expected format for Khronos

--- src/utils/api/common/bets/BetsCacheService.ts
+++ src/utils/api/common/bets/BetsCacheService.ts
@@ -1,26 +1,49 @@
 import { CacheManager } from '@pluto-redis'
-import {
-	IFinalizedBetslip,
-	IPendingBetslip,
-} from '../../../../lib/interfaces/api/bets/betslips.interfaces' // Assuming a generic CacheManager interface
+import { BetslipWithAggregationDTO, PlaceBetDto } from '@khronos-index'
 
 export class BetsCacheService {
 	private cachePrefix = 'bets:'
 
 	constructor(private cache: CacheManager) {}
 
-	async cacheUserBet(userId: string, betData: any) {
+	async cacheUserBet(userId: string, betData: BetslipWithAggregationDTO) {
 		const cacheKey = this.cachePrefix + userId
 		await this.cache.set(cacheKey, betData, 86400) // Cache for 1 day, adjust TTL as needed
-		console.log(`Cached bet for user ${userId}`)
+		console.log(`Cached bet for user ${userId}\nKey: ${cacheKey}`)
 	}
 
 	async getUserBet(
 		userId: string,
-	): Promise<IFinalizedBetslip | IPendingBetslip | null> {
+	): Promise<BetslipWithAggregationDTO | null> {
 		const cacheKey = this.cachePrefix + userId
 		const betData = await this.cache.get(cacheKey)
 		return betData || null
+	}
+
+	async sanitize(betData: any): Promise<PlaceBetDto> {
+		console.debug({
+			method: this.sanitize.name,
+			status: `Pre-parsed`,
+			data: {
+				betData,
+			},
+		})
+		// Organize props to fit expected structure for Khronos
+		betData.matchup_id = betData.match.id
+		// Remove unnecessary data so the bet can be placed
+		delete betData?.match
+		delete betData?.dateofmatchup
+		delete betData?.opponent
+		delete betData?.profit
+		delete betData?.payout
+		console.debug({
+			method: this.sanitize.name,
+			status: `Sanitized`,
+			data: {
+				betData,
+			},
+		})
+		return betData
 	}
 
 	async clearUserBet(userId: string) {
