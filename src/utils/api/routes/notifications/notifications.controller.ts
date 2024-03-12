b8ca6d26700ac800d38b5a128c970f827c9e45ee
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-03-12 15:40:52 -0400

    chore: Add debugging statements for notifications

--- src/utils/api/routes/notifications/notifications.controller.ts
+++ src/utils/api/routes/notifications/notifications.controller.ts
@@ -8,9 +8,18 @@ const NotificationRouter = new Router()
 
 NotificationRouter.post(`/notifications/bets/results`, async (ctx) => {
 	const NotificationData = ctx.request.body || {}
-
+	console.debug({
+		method: `NotificationRouter`,
+		message: 'Notification data received.',
+		data: NotificationData,
+	})
 	// Check if the NotificationData is empty or if it fails validation
 	if (_.isEmpty(NotificationData) || !isNotifyBetUsers(NotificationData)) {
+		console.debug({
+			method: `NotificationRouter`,
+			message: 'Invalid or no notification data was received.',
+			data: NotificationData,
+		})
 		ctx.body = {
 			success: false,
 			error: 'Invalid or no notification data was received.',
