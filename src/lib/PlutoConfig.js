1e34746c5052e7041ddfd4999909e9be1429ace4
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-03-28 12:43:42 -0400

    refactor: Clean config file; Redis auth from .env

--- src/lib/PlutoConfig.js
+++ src/lib/PlutoConfig.js
@@ -1,7 +1,6 @@
 import { Command } from '@sapphire/framework'
 import _ from 'lodash'
-import accounting from 'accounting'
-import { SEASON_TYPE, serverEnv } from '@pluto-server-config'
+import { serverEnv } from '@pluto-server-config'
 import { embedReply, QuickError } from '@pluto-embed-reply'
 import { Log } from '@pluto-internal-logger'
 import { findEmoji } from '../utils/bot_res/findEmoji.js'
@@ -19,81 +18,13 @@ export {
 	yellowBright,
 } from 'colorette'
 
-export const formatCurrency = (
-	value,
-	currency = '$',
-	precision = 0,
-	thousandSeparator = ',',
-	decimalSeparator = '.',
-) =>
-	accounting.formatMoney(
-		value,
-		currency,
-		precision,
-		thousandSeparator,
-		decimalSeparator,
-	)
-
-//  # REVIEW: Pre-season status
-export const isPreSzn = () => {
-	return SEASON_TYPE === 'preseason'
-}
-
 // ? Embed usage
 const helpfooter = 'dev. by fenixforever'
 
 // ? General Config
-export {
-	helpfooter,
-	Command,
-	_,
-	Log,
-	QuickError,
-	embedReply,
-	accounting,
-	findEmoji,
-}
+export { helpfooter, Command, _, Log, QuickError, embedReply, findEmoji }
 
 // # DB Configs
-const {
-	ODDS,
-	SCORE,
-	PENDING,
-	BETSLIPS,
-	CURRENCY,
-	PROFILES,
-	LIVEMATCHUPS,
-	SCORETABLE,
-	LIVEBETS,
-	EXPERIENCE,
-	server_ID,
-	gameCat_ID,
-	sportsLogo,
-	bettingChan,
-	logChan,
-	gameHeartbeat,
-	scheduledGames,
-	getRanges,
-} = serverEnv
+const { logChan } = serverEnv
 
-export {
-	sportsLogo,
-	SCORETABLE,
-	LIVEMATCHUPS,
-	LIVEBETS,
-	BETSLIPS,
-	PROFILES,
-	PENDING,
-	SCORE,
-	EXPERIENCE,
-	ODDS,
-	CURRENCY,
-	SEASON_TYPE,
-	server_ID,
-	gameCat_ID,
-	bettingChan,
-	logChan,
-	gameHeartbeat,
-	scheduledGames,
-	getRanges,
-}
+export { logChan }
---
+++ src/utils/cache/redisInstance.ts
@@ -0,0 +1,35 @@
+import Redis from 'ioredis'
+import { bold, green, yellow } from 'colorette'
+
+const { R_HOST, R_PORT, R_PASS, R_DB } = process.env
+
+console.log(yellow(`Connecting to Redis: ${R_HOST}:${R_PORT}`))
+
+const MAX_RETRY_ATTEMPTS = 2 // Set the maximum number of retry attempts
+
+const redisCache = new Redis({
+	host: R_HOST,
+	port: Number(R_PORT),
+	password: R_PASS,
+	connectTimeout: 10000,
+	retryStrategy: (times) => {
+		if (times >= MAX_RETRY_ATTEMPTS) {
+			throw new Error('Max retry attempts reached')
+		}
+		return Math.min(Math.pow(2, times) * 1000, 60000)
+	},
+	db: Number(R_DB),
+})
+
+redisCache.on('connect', () => {
+	console.log(
+		bold(green(`[REDIS] Connected to Redis server\nDB Selection: ${R_DB}`)),
+	)
+})
+
+redisCache.on('error', (err) => {
+	console.error(err)
+	process.exit(1)
+})
+
+export default redisCache
