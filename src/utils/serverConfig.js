37bfd4e6f9b0b396561ba4173a746091e614362d
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-03-28 12:01:15 -0400

    refactor: Rm deprecated config variables

--- src/utils/serverConfig.js
+++ src/utils/serverConfig.js
@@ -12,77 +12,26 @@ const configData = configResp.config
  * Anything else: camelCase
  */
 const serverEnv = {
-	ODDS: configData.ODDS,
-	SCORE: configData.SCORE,
-	RANGES: configData.RANGES,
-	PENDING: configData.PENDING,
-	BETSLIPS: configData.BETSLIPS,
-	CURRENCY: configData.CURRENCY,
-	LIVEBETS: configData.LIVEBETS,
-	PROFILES: configData.PROFILES,
-	LIVEMATCHUPS: configData.LIVEMATCHUPS,
-	SCORETABLE: configData.SCORETABLE,
-	EXPERIENCE: configData.EXPERIENCE,
-	server_ID: configData.server_ID,
-	gameCat_ID: configData.gameCat_ID,
-	sportsLogo: configData.sportsLogo,
-	bettingChan: configData.bettingChan,
 	logChan: configData.logChan,
-	statcordKey: configData.statcordKey,
-	gameHeartbeat: configData.gameHeartbeat,
-	scheduledGames: configData.scheduledGames,
-	getRanges: configData.getRanges,
-	getOdds: configData.getOdds,
-	SPORT: configData.SPORT,
 	R_HOST: configData.R_HOST,
 	R_PORT: configData.R_PORT,
 	R_DB: configData.R_DB,
 	R_PASS: configData.R_PASS,
-	SEASON_TYPE: configData.SEASON_TYPE,
-	admin_token: configData.admin_token,
 	pluto_api_url: configData.pluto_api_url,
-	pluto_api_username: configData.pluto_api_username,
 }
 
 const {
 	// DB Tables
-	BETSLIPS,
-	CURRENCY,
 	// IDs
-	server_ID,
-	gameCat_ID,
-	bettingChan,
 	logChan,
 	// Settings
-	gameHeartbeat,
-	SPORT,
 	R_HOST,
 	R_PORT,
 	R_DB,
 	R_PASS,
-	SEASON_TYPE,
-	admin_token,
 	pluto_api_url,
-	pluto_api_username,
 } = serverEnv
 
-export {
-	BETSLIPS,
-	CURRENCY,
-	server_ID,
-	gameCat_ID,
-	bettingChan,
-	logChan,
-	gameHeartbeat,
-	SPORT,
-	R_HOST,
-	R_PORT,
-	R_DB,
-	R_PASS,
-	SEASON_TYPE,
-	admin_token,
-	pluto_api_url,
-	pluto_api_username,
-}
+export { logChan, R_HOST, R_PORT, R_DB, R_PASS, pluto_api_url }
 
 export { configData as serverConf, serverEnv }
