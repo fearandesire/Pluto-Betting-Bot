d6dc07c24ee62b21b87f5adb72d3667d54e35da8
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-04-19 18:58:50 -0400

    style: Adjust Patreon msg formatting

--- src/utils/api/patreon/interfaces.ts
+++ src/utils/api/patreon/interfaces.ts
@@ -4,7 +4,7 @@ export const nonPatreonMemberMsg = `This feature is only for Patreon members!\nS
 
 export const patreonFooterMsg = `Support continued development via Patreon | Use \`/patreon\` to learn more`
 
-export const PatreonInformation = `Pluto is completely free to use. However, it is not free to run or maintain, not to mention the time investment! Additionally, there's several key features that are a long-term goal to add, including the highly requested Parlay feature.\n\nBecoming a Patreon member and supporting Pluto will enable Pluto to grow to it\'s full potential of being the best sports betting app on Discord. New features will be added, including:\n- 🎲 **Parlay Bets**\n- 🕛 **Real-Time Game Data**\n- 👴 **Historical Game Data**\n..and more!\n\nTo support Pluto and become a Patreon member, please click [here](${patreonUrl}).\n\nThank you!\n \- fenix 🎔`
+export const PatreonInformation = `Pluto is completely free to use. However, it is not free to run or maintain, not to mention the time investment! Additionally, there's several key features that are a long-term goal to add, including the highly requested Parlay feature.\n\nBecoming a Patreon member and supporting Pluto will enable Pluto to grow to it\'s full potential of being the best sports betting app on Discord. New features will be added, including:\n- 🎲 **Parlay Bets**\n- 🕛 **Real-Time Game Data**\n- 👴 **Historical Game Data**\n..and more!\n\nSupport Pluto via Patreon 🡺 please click [**here**](${patreonUrl}).\n\nThank you!\n \- fenix 🎔`
 
 export const patreonFooterUrl = `https://i.imgur.com/qG3Mm5t.png`
 
