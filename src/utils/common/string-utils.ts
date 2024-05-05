9a99fb1426738456d563f474c396783bba8d4672
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-05-05 19:41:32 -0400

    refactor: Ensure return type is string

--- src/utils/common/string-utils.ts
+++ src/utils/common/string-utils.ts
@@ -1,10 +1,11 @@
 import _ from 'lodash'
 
 export default class StringUtils {
-	getShortName(name: string) {
+	getShortName(name: string): string {
 		if (name.includes(' ')) {
-			return _.last(_.split(name, ' '))
+			return _.last(_.split(name, ' ')) || name
+		} else {
+			return name
 		}
-		return name
 	}
 }
