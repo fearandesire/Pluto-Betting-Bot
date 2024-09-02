910f4ebae0c20dc62b65bb1d8ad48a2401943abe
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-09-02 12:16:25 -0400

    refactor: gracefully handle startup err

--- src/lib/startup/cache.ts
+++ src/lib/startup/cache.ts
@@ -5,12 +5,28 @@ import { CacheManager } from '@pluto-redis'
  * Store matches in cache on startup
  */
 async function initCache() {
-	const matchCacheService = new MatchCacheService(new CacheManager())
-	const data = await matchCacheService.requestMatches()
-	if (data?.matches && data.matches.length > 0) {
-		await matchCacheService.cacheMatches(data.matches)
-	} else {
-		console.error(`Failed to cache matches | No matches were returned.`)
+	try {
+		const matchCacheService = new MatchCacheService(new CacheManager())
+		const data = await matchCacheService.requestMatches()
+		if (data?.matches && data.matches.length > 0) {
+			await matchCacheService.cacheMatches(data.matches)
+		} else {
+			console.error('Failed to cache matches | No matches were returned.')
+		}
+	} catch (error) {
+		if (error instanceof Error) {
+			if ('response' in error) {
+				console.error(
+					'Failed to cache matches | API error:',
+					(error.response as Response).status,
+					(error.response as Response).statusText,
+				)
+			} else {
+				console.error('Failed to cache matches | Error:', error.message)
+			}
+		} else {
+			console.error('Failed to cache matches | Unknown error:', error)
+		}
 	}
 }
 
