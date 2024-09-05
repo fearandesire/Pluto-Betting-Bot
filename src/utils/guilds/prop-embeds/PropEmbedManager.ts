65b4f86b706ac4e5e7013807a2990db83f8c43d0
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-09-05 14:41:09 -0400

    refactor: use new translations; format prop embed

--- src/utils/guilds/prop-embeds/PropEmbedManager.ts
+++ src/utils/guilds/prop-embeds/PropEmbedManager.ts
@@ -6,6 +6,8 @@ import {
 	EmbedBuilder,
 } from 'discord.js'
 import GuildUtils from '../GuildUtils.js'
+import { MarketKeyTranslations } from '../../api/common/interfaces'
+import { formatDiscordTimestamp } from '../../timestampUtils.js'
 
 export default class PropEmbedManager {
 	async createEmbeds(
@@ -18,62 +20,56 @@ export default class PropEmbedManager {
 				props,
 			)
 
-			const embedDetails = {
-				title: `Accuracy Challenge`,
-			}
-
 			// Create an embed for each prop
 			const embeds = props.map((prop) => {
+				const marketKey = prop.market_key // Assuming prop has a market_key property
+				const marketDescription =
+					// @ts-ignore - Not supporting every market. Will need to narrow this down.
+					MarketKeyTranslations[marketKey] || marketKey // Use the enum for translation
+				const descriptionStr = `Will ${prop?.description} get over/under ${prop.point} ${marketDescription}?`
+
+				const embedDetails = {
+					title: `Accuracy Challenge`,
+					desc: descriptionStr,
+				}
+
 				const embed = new EmbedBuilder()
 					.setTitle(embedDetails.title)
-					.setDescription(
-						`Will ${prop?.description} get over/under ${prop.point} <market>?`,
-					)
+					.setDescription(embedDetails.desc)
 					.addFields(
 						{
-							name: 'Home Team',
-							value: prop.home_team,
+							name: 'Match',
+							value: `${prop.home_team} vs ${prop.away_team}`,
 							inline: true,
 						},
 						{
-							name: 'Away Team',
-							value: prop.away_team,
-							inline: true,
-						},
-						// Bar for player to get over/under [TODO: Translate into what the requirement is based on the market key]
-						{
-							name: 'Points',
-							value: `${prop.point}`,
+							name: 'Date',
+							value: formatDiscordTimestamp(prop.commence_time),
 							inline: true,
 						},
 					)
 					.setFooter({ text: `ID: ${prop.id}` })
 					.setColor('#0099ff')
 					.setTimestamp()
-
-				// Create buttons for over/under
+				// Create buttons
 				const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
 					new ButtonBuilder()
 						.setCustomId(`prop_bet_over_${prop.id}`)
-						.setLabel(`Over ${prop.point}`)
+						.setLabel(`Over`)
 						.setStyle(ButtonStyle.Primary),
 					new ButtonBuilder()
 						.setCustomId(`prop_bet_under_${prop.id}`)
-						.setLabel(`Under ${prop.point}`)
+						.setLabel(`Under`)
 						.setStyle(ButtonStyle.Primary),
 				)
-
 				return { embed, row }
 			})
-
 			const guildUtils = new GuildUtils()
 			const guild = await guildUtils.getGuild(guild_id)
-
 			if (!guild) {
-				console.error(`Guild not found: ${guild_id}`)
+				console.error(`[PropEmbedManager] Guild not found: ${guild_id}`)
 				continue
 			}
-
 			const channel = await guild.channels.fetch(prop_channel_id)
 			if (channel && channel.isTextBased()) {
 				for (const { embed, row } of embeds) {
