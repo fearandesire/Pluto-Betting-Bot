de28c3ea5c7ce6128d547aa30240795719f1d851
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-09-05 14:41:22 -0400

    refactor: export translations

--- src/utils/api/common/interfaces/index.ts
+++ src/utils/api/common/interfaces/index.ts
@@ -1,4 +1,5 @@
 export * from './common-interfaces'
+export * from './market-translations'
 export * from './schemas/betting-market.schema'
 export * from './schemas/prop.schema'
 export * from './schemas/standard.zod.interface'
