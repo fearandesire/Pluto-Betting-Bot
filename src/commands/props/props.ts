dd3488fa96241fa8ba595b80c6db1cc61a316207
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-09-30 20:46:22 -0400

    refactor: cmd desc, ids

--- src/commands/props/props.ts
+++ src/commands/props/props.ts
@@ -1,16 +1,23 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
+import { PermissionFlagsBits } from 'discord.js';
 import PropsApiWrapper from '../../utils/api/Khronos/props/propsApiWrapper.js';
 
 @ApplyOptions<Command.Options>({
-	description: 'A basic slash command',
+	description: 'Generate all prop embeds (Admin/Mod only)',
 })
 export class UserCommand extends Command {
 	public override registerApplicationCommands(registry: Command.Registry) {
-		registry.registerChatInputCommand((builder) =>
-			builder //
-				.setName(this.name)
-				.setDescription(this.description),
+		registry.registerChatInputCommand(
+			(builder) =>
+				builder //
+					.setName(this.name)
+					.setDescription(this.description)
+					.setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
+			{
+				idHints: ['1288178546942021643', '1290465537859784745'],
+				guildIds: ['777353407383339038'],
+			},
 		);
 	}
 
