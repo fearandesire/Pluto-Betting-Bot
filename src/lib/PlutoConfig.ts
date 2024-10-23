759226c1fefe23fd35836671486125076985ac19
fearandesire <fenixcoding@gmail.com>
2024-10-23 13:19:56 -0400

    refactor: expand footers

--- src/lib/PlutoConfig.ts
+++ src/lib/PlutoConfig.ts
@@ -21,6 +21,19 @@ const footers = [
 	'💲 Use /balance to check your profile & level',
 	'👁️ Use /commands to see all commands',
 	'❓ Learn more about Pluto via /help',
+	'🃏 Addicted? Call 1-800-GAMBLER',
+];
+
+const bettingFooters = [
+	'🦄 Unicorns are real, and so are your chances of winning... maybe',
+	"🐳 Whale alert! Oh wait, that's just your ambition",
+	"🔮 Our crystal ball says you'll win... but it's been wrong before",
+	'🚀 To the moon! Or at least to the next payout',
+	'🦸‍♀️ You are guaranteed to win! (results may vary)',
+];
+
+const placedBetFooters = [
+	'🧙‍♂️ Abracadabra! Your money has magically disappeared!',
 ];
 
 function helpfooter() {
