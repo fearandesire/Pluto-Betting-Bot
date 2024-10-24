c62998e2ff685444b570a718741c6602869940c9
fearandesire <fenixcoding@gmail.com>
2024-10-24 09:11:46 -0400

    refactor: more debug logging

--- src/utils/api/patreon/PatreonManager.ts
+++ src/utils/api/patreon/PatreonManager.ts
@@ -1,3 +1,4 @@
+import { AxiosError } from 'axios';
 import type { IApiError } from '../../../lib/interfaces/errors/api-errors.js';
 import { patreonApiInstance } from './PatreonInstance.js';
 import { type IPatreonReadUser, nonPatreonMemberMsg } from './interfaces.js';
@@ -15,10 +16,16 @@ export default class PatreonManager {
 			const res = await this.patreonApi.get(`/read/${userid}`);
 			return res.data as IPatreonReadUser;
 		} catch (error) {
-			console.error({
-				message: `[${this.reqPatreonUserData.name}] Error`,
-				error,
-			});
+			let respData: any;
+			// Log Axios error
+			if (error instanceof AxiosError) {
+				respData = error.response?.data;
+				console.error({
+					message: `[${this.reqPatreonUserData.name}] Error`,
+					respData,
+					error,
+				});
+			}
 			return {
 				message: 'Failed to fetch Patreon user data',
 				metadata: {
