957b00fe2b9a10e092903f9a648ca53fd70c30fd
FENIX <63979371+fearandesire@users.noreply.github.com>
2025-12-29 22:07:14 -0500

    fix: update environment variable validation (#312)
    * fix: update environment variable validation for PATREON_API_URL and KH_PLUTO_CLIENT_KEY
    
    * fix: enhance environment variable validation and update usage in Axios configuration
    
    * fix: improve formatting of environment variable validation messages for consistency
    
    * fix: streamline environment variable validation formatting for consistency
    
    * Merge remote-tracking branch 'origin/main' into fix/env-validation
    
    * fix: enhance environment variable validation by adding required constraints for several fields
    
    * fix: improve formatting of environment variable validation for consistency across all fields

--- src/utils/api/common/axios-config.ts
+++ src/utils/api/common/axios-config.ts
@@ -1,14 +1,15 @@
 import axios, { AxiosHeaders } from 'axios'
+import env from '../../../lib/startup/env.js'
 
 /**
  * Axios Instance(s) configured for our needs to reduce redundant specifications/setup
  */
 
 const KhronosStandardConfig = {
-	baseURL: `${process.env.KH_API_URL}`,
+	baseURL: `${env.KH_API_URL}`,
 	timeout: 90000, // 90 Seconds in MS
 	headers: new AxiosHeaders({
-		'admin-token': `${process.env.KH_API_TOKEN}`,
+		'admin-token': `${env.KH_API_TOKEN}`,
 	}),
 }
 
