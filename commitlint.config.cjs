213ea5a9d2d1fd5e78adf1bca950f49b5a48d758
fearandesire <fenixcoding@gmail.com>
2026-01-28 17:40:45 -0500

    chore(commitlint): add ignore rule for merge commit messages

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,28 +1,33 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'patch',
-        'hotfix',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'build',
-        'ci',
-        'chore',
-        'revert',
-      ],
-    ],
-    'subject-empty': [2, 'never'],
-    'header-max-length': [2, 'always', 100],
-    'body-max-line-length': [1, 'always', 100],
-  },
-};
-
+	extends: ['@commitlint/config-conventional'],
+	ignores: [
+		(message) => message.includes('Merge branch'),
+		(message) => message.includes('Merge pull request'),
+		(message) => /^(fixup|squash)!/.test(message),
+		(message) => message.includes('Signed-off-by: dependabot[bot]'),
+		(message) => message.includes('chore(release):'),
+	],
+	rules: {
+		'type-enum': [
+			2,
+			'always',
+			[
+				'feat',
+				'fix',
+				'docs',
+				'style',
+				'refactor',
+				'perf',
+				'test',
+				'build',
+				'ci',
+				'chore',
+				'revert',
+			],
+		],
+		'subject-empty': [2, 'never'],
+		'subject-case': [2, 'never', ['upper-case', 'pascal-case']],
+		'header-max-length': [2, 'always', 100],
+		'body-max-line-length': [1, 'always', 100],
+	},
+}
