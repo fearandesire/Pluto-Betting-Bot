3716406363950bcc5e513f4882469e50cd0aa199
FENIX <63979371+fearandesire@users.noreply.github.com>
2026-01-28 00:23:37 -0500

    feat: add timezone support to odds command and update dependencies (#370)
    Co-authored-by: Ronald Mathews <ron.mathews.ucf@gmail.com>

--- src/commands/betting/odds.ts
+++ src/commands/betting/odds.ts
@@ -19,7 +19,15 @@ export class UserCommand extends Command {
 			builder //
 				.setName(this.name)
 				.setDescription(this.description)
-				.setContexts(InteractionContextType.Guild),
+				.setContexts(InteractionContextType.Guild)
+				.addStringOption((option) =>
+					option
+						.setName('timezone')
+						.setDescription(
+							'The time zone displayed for the matches, in IANA format (e.g., America/New_York)',
+						)
+						.setRequired(false),
+				),
 		)
 	}
 
@@ -59,12 +67,28 @@ export class UserCommand extends Command {
 				matchCount: matches?.length ?? 0,
 			})
 
+			const userTZInput = interaction.options
+				.getString('timezone', false)
+				?.trim()
+			if (userTZInput) {
+				try {
+					new Date().toLocaleString('en-US', {
+						timeZone: userTZInput,
+					})
+				} catch {
+					return interaction.editReply({
+						content:
+							'Invalid timezone, please use an IANA format like America/New_York.',
+					})
+				}
+			}
 			let oddsEmbed
 			try {
 				oddsEmbed = await prepareAndFormat(
 					matches,
 					embedThumbnail,
 					guildId,
+					userTZInput,
 				)
 			} catch (formatError) {
 				logger.debug('Odds command: error in prepareAndFormat', {
